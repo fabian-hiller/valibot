fd6d9d4f2697aa12d10cb09b5708e115ccc7283d
Fabian Hiller <hillerfabian11@gmail.com>
2023-07-11 17:31:47 +0200

    Add size, minSize, maxSize and mimeType validation

---
+++ library/src/validations/mimeType.ts
@@ -0,0 +1,30 @@
+import { ValiError } from '../error';
+import type { ValidateInfo } from '../types';
+
+/**
+ * Creates a validation functions that validates the MIME type of a file.
+ *
+ * @param requirement The MIME types.
+ * @param error The error message.
+ *
+ * @returns A validation function.
+ */
+export function mimeType<TInput extends File>(
+  requirement: string[],
+  error?: string
+) {
+  return (input: TInput, info: ValidateInfo) => {
+    if (!requirement.includes(input.type)) {
+      throw new ValiError([
+        {
+          validation: 'mime_type',
+          origin: 'value',
+          message: error || 'Invalid MIME type',
+          input,
+          ...info,
+        },
+      ]);
+    }
+    return input;
+  };
+}
---
+++ library/src/validations/size.ts
@@ -0,0 +1,30 @@
+import { ValiError } from '../error';
+import type { ValidateInfo } from '../types';
+
+/**
+ * Creates a validation functions that validates the size of a map, set or blob.
+ *
+ * @param requirement The size.
+ * @param error The error message.
+ *
+ * @returns A validation function.
+ */
+export function size<TInput extends Map<any, any> | Set<any> | Blob>(
+  requirement: number,
+  error?: string
+) {
+  return (input: TInput, info: ValidateInfo) => {
+    if (input.size !== requirement) {
+      throw new ValiError([
+        {
+          validation: 'size',
+          origin: 'value',
+          message: error || 'Invalid size',
+          input,
+          ...info,
+        },
+      ]);
+    }
+    return input;
+  };
+}
