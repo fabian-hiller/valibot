fdc1254a101b30c312d264b2db46f2507c644fb9
Fabian Hiller <hillerfabian11@gmail.com>
2023-07-22 13:35:31 +0200

    Add root CSS, Tailwind config and fonts to website

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lexend: ['"Lexend"', ...fontFamily.sans],
+        'lexend-exa': ['"Lexend Exa"', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
