2f5dc79930ae66a502d66f0f5c0a5fa80e9aae52
Fabian Hiller <hillerfabian11@gmail.com>
2023-07-24 00:23:44 +0200

    Add base layout of website

--- website/src/routes/layout.tsx
+++ website/src/routes/layout.tsx
@@ -1,5 +1,20 @@
 import { component$, Slot } from '@builder.io/qwik';
+import { type DocumentHead } from '@builder.io/qwik-city';
+import { Footer, Header, RoutingIndicator } from '~/components';
 
-export default component$(() => {
-  return <Slot />;
+export default component$(() => (
+  <>
+    <RoutingIndicator />
+    <Header />
+    <Slot />
+    <Footer />
+  </>
+));
+
+export const head: DocumentHead = (request) => ({
+  // Add name of site to title of subpages
+  title:
+    request.url.pathname === '/'
+      ? request.head.title
+      : `${request.head.title} | Valibot`,
 });
