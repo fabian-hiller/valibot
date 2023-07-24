dea898453b7a1728dc2487863d328236b268ae87
Genki Takiuchi <genki@s21g.com>
2023-07-24 18:41:10 +0900

    Added a validator `maxBytes` that validates string length in byte

---
+++ library/src/validations/maxBytes/maxBytes.ts
@@ -0,0 +1,31 @@
+import { ValiError } from '../../error';
+import type { ValidateInfo } from '../../types';
+
+/**
+ * Creates a validation functions that validates the byte length of a string.
+ *
+ * @param requirement The maximum length in byte.
+ * @param error The error message.
+ *
+ * @returns A validation function.
+ */
+export function maxBytes<TInput extends string>(
+  requirement: number,
+  error?: string
+) {
+  return (input: TInput, info: ValidateInfo) => {
+    const bytes = encodeURI(input).replace(/%../g, 'x').length;
+    if (bytes > requirement) {
+      throw new ValiError([
+        {
+          validation: 'max_bytes',
+          origin: 'value',
+          message: error || 'Invalid byte length',
+          input,
+          ...info,
+        },
+      ]);
+    }
+    return input;
+  };
+}
