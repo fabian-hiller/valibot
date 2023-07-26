97754b1eacaf97ac706705138598be2e9f644303
userquin <userquin@gmail.com>
2023-07-26 15:18:09 +0200

    fix: types

---
+++ library/build.config.ts
@@ -0,0 +1,42 @@
+import { type BuildEntry, defineBuildConfig } from 'unbuild';
+
+import packageJSON from './package.json';
+
+const entries: BuildEntry[] = [];
+const exportsList = packageJSON['exports'];
+const match = '.';
+Object.keys(exportsList).forEach((key) => {
+  if (key.slice(0, match.length) !== match) {
+    return;
+  }
+
+  const importValue = exportsList[key]['import'];
+  if (key === '.') {
+    entries.push({
+      input: 'src/index',
+      name: 'index',
+    });
+    return;
+  }
+  key = key.slice(match.length + 1);
+  if (importValue === './dist/' + key + '.mjs') {
+    entries.push({
+      input: 'src/' + key + '/index',
+      name: key,
+    });
+  }
+});
+
+export default defineBuildConfig({
+  outDir: './dist',
+  entries,
+  declaration: true,
+  clean: true,
+  rollup: {
+    emitCJS: true,
+    dts: {
+      respectExternal: true,
+    },
+  },
+  externals: ['vitest'],
+});
