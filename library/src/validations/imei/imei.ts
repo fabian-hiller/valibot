ea8773c85296396347c79532a9dfe17882b9fc64
Daniel Winkelmann <dwinkelmann@outlook.com>
2023-08-01 15:23:02 +0200

    feat: add comments

--- library/src/validations/imei/imei.ts
+++ library/src/validations/imei/imei.ts
@@ -2,7 +2,11 @@ import { ValiError } from '../../error/index.ts';
 import type { ValidateInfo } from '../../types.ts';
 
 /**
- * Creates a validation functions that validates a imei.
+ * Creates a validation functions that validates a IMEI (International Mobile Equipment Identity).
+ * 
+ * INFO: https://en.wikipedia.org/wiki/International_Mobile_Equipment_Identity
+ * 
+ * Algorithm: https://en.wikipedia.org/wiki/Luhn_algorithm
  *
  * @param error The error message.
  *
@@ -15,7 +19,7 @@ export function imei<TInput extends string>(error?: string) {
         {
           validation: 'imei',
           origin: 'value',
-          message: error || 'Invalid imei',
+          message: error || 'Invalid IMEI',
           input,
           ...info,
         },
@@ -27,9 +31,12 @@ export function imei<TInput extends string>(error?: string) {
 
 function isValidImei(imei: string): boolean {
 
+  // IMEI must be 15 digits long
   if (imei.length !== 15) {
     return false
   }
+
+  // IMEI must contain only digits
   const checkDigit = +imei[14]
   if (isNaN(checkDigit)) {
     return false
@@ -38,9 +45,11 @@ function isValidImei(imei: string): boolean {
   if (restDigits.some(v => isNaN(v))) {
     return false
   }
+  // IMEI must pass Luhn algorithm
   const everySecondDigitDoubled = restDigits.map((v, i) => i % 2 ? v * 2 : v)
   const everyDoubledNumberSplitted = everySecondDigitDoubled.join('').split('').map(v => +v)
   const sum = everyDoubledNumberSplitted.reduce((a, b) => a + b, 0)
+  // The check digit (x) is obtained by computing the sum of digits then computing 10 - (sum of digits modulo 10)
   const calculatedCheckDigit = (10 - (sum % 10)) % 10
   return checkDigit === calculatedCheckDigit
 }
