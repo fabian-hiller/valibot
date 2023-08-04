e4b28ece4ad52ad862a7b1c2d8020f373d2afc87
Fabian Hiller <hillerfabian11@gmail.com>
2023-08-04 12:54:08 +0200

    Fix tests of validations with ISO time regex #42

--- library/src/validations/isoTime/isoTime.test.ts
+++ library/src/validations/isoTime/isoTime.test.ts
@@ -8,16 +8,20 @@ describe('isoTime', () => {
     const validate = isoTime();
     const value1 = '19:34';
     expect(validate(value1, info)).toBe(value1);
-    const value2 = '01:00';
+    const value2 = '00:00';
     expect(validate(value2, info)).toBe(value2);
     const value3 = '23:59';
     expect(validate(value3, info)).toBe(value3);
 
     expect(() => validate('', info)).toThrowError();
     expect(() => validate('1:34', info)).toThrowError();
-    expect(() => validate('00:00', info)).toThrowError();
-    expect(() => validate('25:00', info)).toThrowError();
+    expect(() => validate('24:00', info)).toThrowError();
     expect(() => validate('01:60', info)).toThrowError();
+    expect(() => validate('99:99', info)).toThrowError();
+    expect(() => validate('0:00', info)).toThrowError();
+    expect(() => validate('00:0', info)).toThrowError();
+    expect(() => validate('000:00', info)).toThrowError();
+    expect(() => validate('00:000', info)).toThrowError();
   });
 
   test('should throw custom error', () => {
--- library/src/validations/isoTimeSecond/isoTimeSecond.test.ts
+++ library/src/validations/isoTimeSecond/isoTimeSecond.test.ts
@@ -8,17 +8,23 @@ describe('isoTimeSecond', () => {
     const validate = isoTimeSecond();
     const value1 = '19:34:12';
     expect(validate(value1, info)).toBe(value1);
-    const value2 = '01:00:00';
+    const value2 = '00:00:00';
     expect(validate(value2, info)).toBe(value2);
     const value3 = '23:59:59';
     expect(validate(value3, info)).toBe(value3);
 
     expect(() => validate('', info)).toThrowError();
     expect(() => validate('1:34:12', info)).toThrowError();
-    expect(() => validate('00:00:00', info)).toThrowError();
-    expect(() => validate('25:00:00', info)).toThrowError();
+    expect(() => validate('0:00:00', info)).toThrowError();
+    expect(() => validate('00:0:00', info)).toThrowError();
+    expect(() => validate('00:00:0', info)).toThrowError();
+    expect(() => validate('000:00:00', info)).toThrowError();
+    expect(() => validate('00:000:00', info)).toThrowError();
+    expect(() => validate('00:00:000', info)).toThrowError();
+    expect(() => validate('24:00:00', info)).toThrowError();
     expect(() => validate('01:60:00', info)).toThrowError();
     expect(() => validate('01:00:60', info)).toThrowError();
+    expect(() => validate('99:99:99', info)).toThrowError();
   });
 
   test('should throw custom error', () => {
