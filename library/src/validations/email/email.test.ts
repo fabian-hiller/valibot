2bb74281d6ab1542c030baa31c9e997c2cb126c3
Fabian Hiller <hillerfabian11@gmail.com>
2023-10-06 01:12:13 -0400

    Simplify email regex and improve tests

--- library/src/validations/email/email.test.ts
+++ library/src/validations/email/email.test.ts
@@ -4,32 +4,31 @@ import { email } from './email.ts';
 describe('email', () => {
   test('should pass only emails', () => {
     const validate = email();
+
     const value1 = 'email@example.com';
     expect(validate(value1).output).toBe(value1);
-    const value2 = 'firstname.lastname@example.com';
+    const value2 = '1234567890@example.com';
     expect(validate(value2).output).toBe(value2);
-    const value3 = 'email@subdomain.example.com';
+    const value3 = '_______@example.com';
     expect(validate(value3).output).toBe(value3);
-    const value4 = 'firstname+lastname@example.com';
+    const value4 = 'firstname.lastname@example.com';
     expect(validate(value4).output).toBe(value4);
-    const value5 = 'email@123.123.123.123';
+    const value5 = 'firstname-lastname@example.com';
     expect(validate(value5).output).toBe(value5);
-    const value6 = '“email”@example.com';
+    const value6 = 'firstname+lastname@example.com';
     expect(validate(value6).output).toBe(value6);
-    const value7 = '1234567890@example.com';
+    const value7 = 'email@aaa-bbb.com';
     expect(validate(value7).output).toBe(value7);
-    const value8 = 'email@example-one.com';
+    const value8 = 'email@example.name';
     expect(validate(value8).output).toBe(value8);
-    const value9 = '_______@example.com';
+    const value9 = 'email@example.co.uk';
     expect(validate(value9).output).toBe(value9);
-    const value10 = 'email@example.name';
+    const value10 = 'email@subdomain.example.com';
     expect(validate(value10).output).toBe(value10);
-    const value11 = 'email@example.co.uk';
+    const value11 = 'email@subdomain.aaa-bbb.com';
     expect(validate(value11).output).toBe(value11);
-    const value12 = 'firstname-lastname@example.com';
+    const value12 = 'email@subdomain.example.co.uk';
     expect(validate(value12).output).toBe(value12);
-    const value13 = 'email@subdomain.subdomain.example.de';
-    expect(validate(value13).output).toBe(value13);
 
     expect(validate('plainaddress').issues).toBeTruthy();
     expect(validate('#@%^%#$@#$@#.com').issues).toBeTruthy();
