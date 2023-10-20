c8fc071cf4078fa3d5155b7f0c2f14e3ca29f40e
Jacob Groß <kurtextrem@gmail.com>
2023-10-20 15:59:35 +0200

    Update ipv4 regex
    fastest variant

--- library/src/validations/ipv4/ipv4.ts
+++ library/src/validations/ipv4/ipv4.ts
@@ -10,7 +10,7 @@ import { getOutput, getPipeIssues } from '../../utils/index.ts';
  */
 export function ipv4<TInput extends string>(error?: ErrorMessage) {
   return (input: TInput): PipeResult<TInput> =>
-    !/^(?:(?:(?=(25[0-5]))\1|(?=(2[0-4]\d))\2|(?=(1\d{2}))\3|(?=(\d{1,2}))\4)\.){3}(?:(?=(25[0-5]))\5|(?=(2[0-4]\d))\6|(?=(1\d{2}))\7|(?=(\d{1,2}))\8)$/u.test(
+    !/^(?:(?:25[0-5]|(?:2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/u.test(
       input
     )
       ? getPipeIssues('ipv4', error || 'Invalid IP v4', input)
