87ca05f4ee8a65d1c26223c88955255a319a2440
kazizi55 <kaz.redhot1201@gmail.com>
2023-12-29 20:12:02 +0900

    docs: add number

---
+++ website/src/routes/api/(schemas)/number/properties.ts
@@ -0,0 +1,38 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  message: {
+    type: [
+      {
+        type: 'custom',
+        name: 'ErrorMessage',
+        href: '../ErrorMessage/',
+      },
+      'undefined',
+    ],
+    default: {
+      type: 'string',
+      value: 'Invalid type',
+    },
+  },
+  pipe: {
+    type: [
+      {
+        type: 'custom',
+        name: 'Pipe',
+        href: '../Pipe/',
+        generics: ['number'],
+      },
+      'undefined',
+    ],
+  },
+  Schema: {
+    type: [
+      {
+        type: 'custom',
+        name: 'NumberSchema',
+        href: '../NumberSchema/',
+      },
+    ],
+  },
+};
