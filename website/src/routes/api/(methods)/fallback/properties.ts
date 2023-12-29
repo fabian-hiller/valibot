9eae9e9858174e8c8d602f75b78b9c0a34976693
sqmasep <leblond.romain@orange.fr>
2023-12-29 18:13:18 +0100

    docs(api): Fallback method docs

---
+++ website/src/routes/api/(methods)/fallback/properties.ts
@@ -0,0 +1,78 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TSchema: {
+    type: {
+      type: 'custom',
+      name: 'BaseSchema',
+      href: '../BaseSchema/',
+    },
+  },
+  TFallback: {
+    type: [
+      {
+        type: 'custom',
+        name: 'Output',
+        href: '../Output/',
+        generics: [
+          {
+            type: 'custom',
+            name: 'TSchema',
+          },
+        ],
+      },
+      {
+        type: 'function',
+        params: [
+          {
+            type: {
+              type: 'custom',
+              name: 'FallbackInfo',
+              href: '../FallbackInfo/',
+            },
+            name: 'info',
+            optional: true,
+          },
+        ],
+        return: [
+          {
+            type: 'custom',
+            name: 'Output',
+            href: '../Output/',
+            generics: [
+              {
+                type: 'custom',
+                name: 'TSchema',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  schema: {
+    type: [
+      {
+        type: 'custom',
+        name: 'TWrapped',
+      },
+    ],
+  },
+  fallback: {
+    type: [
+      {
+        type: 'custom',
+        name: 'TDefault',
+      },
+    ],
+  },
+  SchemaWithFallback: {
+    type: [
+      {
+        type: 'custom',
+        name: 'SchemaWithFallback',
+        href: '../SchemaWithFallback/',
+      },
+    ],
+  },
+};
