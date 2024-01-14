264d720d4066200d5bd4f75386631d2cb402e511
Aris Kemper <aris.github@gmail.com>
2024-01-14 11:58:40 +0100

    refactor: string schema tests

--- library/src/schemas/string/string.test.ts
+++ library/src/schemas/string/string.test.ts
@@ -4,14 +4,30 @@ import { email, maxLength, minLength } from '../../validations/index.ts';
 import { string } from './string.ts';
 
 describe('string', () => {
-  test('should pass only strings', () => {
+  describe('should pass', () => {
     const schema = string();
-    const input = '';
-    const output = parse(schema, input);
-    expect(output).toBe(input);
-    expect(() => parse(schema, 123n)).toThrowError();
-    expect(() => parse(schema, null)).toThrowError();
-    expect(() => parse(schema, {})).toThrowError();
+
+    test('empty string schema', () => {
+      const input = '';
+      const output = parse(schema, input);
+      expect(output).toBe(input);
+    });
+  });
+
+  describe('shoud reject', () => {
+    const schema = string();
+
+    test('schema, which imcludes numbers', () => {
+      expect(() => parse(schema, 123)).toThrowError();
+    });
+
+    test('reject null schema', () => {
+      expect(() => parse(schema, null)).toThrowError();
+    });
+
+    test('empty object schema', () => {
+      expect(() => parse(schema, {})).toThrowError();
+    });
   });
 
   test('should throw custom error', () => {
@@ -20,38 +36,62 @@ describe('string', () => {
   });
 
   test('should execute pipe', () => {
-    const lengthError = 'Invalid length';
-    const schema1 = string([minLength(1), maxLength(3)]);
-    const input1 = '12';
-    const output1 = parse(schema1, input1);
-    expect(output1).toBe(input1);
-    expect(() => parse(schema1, '')).toThrowError(lengthError);
-    expect(() => parse(schema1, '1234')).toThrowError(lengthError);
+    describe('minLength & maxLength', () => {
+      const lengthError = 'Invalid length';
+      const schema1 = string([minLength(1), maxLength(3)]);
 
-    const emailError = 'Invalid email';
-    const schema2 = string('Error', [email()]);
-    const input2 = 'jane@example.com';
-    const output2 = parse(schema2, input2);
-    expect(output2).toBe(input2);
-    expect(() => parse(schema2, 'jane@example')).toThrowError(emailError);
+      test('should pass correct length', () => {
+        const input1 = '12';
+        const output1 = parse(schema1, input1);
+        expect(output1).toBe(input1);
+      });
+
+      test('should reject empty string', () => {
+        expect(() => parse(schema1, '')).toThrowError(lengthError);
+      });
+
+      test('should reject to long strings', () => {
+        expect(() => parse(schema1, '1234')).toThrowError(lengthError);
+      });
+    });
+
+    describe('email', () => {
+      const emailError = 'Invalid email';
+      const schema2 = string('Error', [email()]);
+
+      test('should pass', () => {
+        const input2 = 'jane@example.com';
+        const output2 = parse(schema2, input2);
+        expect(output2).toBe(input2);
+      });
+
+      test('should reject invalid email address string', () => {
+        expect(() => parse(schema2, 'jane@example')).toThrowError(emailError);
+      });
+    });
   });
 
-  test('should expose the pipeline', () => {
+  describe('schema pipeline', () => {
     const schema1 = string([minLength(2), maxLength(3)]);
-    expect(schema1.pipe).toStrictEqual([
-      expect.objectContaining({
-        type: 'min_length',
-        requirement: 2,
-        message: 'Invalid length',
-      }),
-      expect.objectContaining({
-        type: 'max_length',
-        requirement: 3,
-        message: 'Invalid length',
-      }),
-    ]);
 
-    const schema2 = string();
-    expect(schema2.pipe).toBeUndefined();
+    test('should contain invalid length message, type and requirements', () => {
+      expect(schema1.pipe).toStrictEqual([
+        expect.objectContaining({
+          type: 'min_length',
+          requirement: 2,
+          message: 'Invalid length',
+        }),
+        expect.objectContaining({
+          type: 'max_length',
+          requirement: 3,
+          message: 'Invalid length',
+        }),
+      ]);
+    });
+
+    test('should be undefined, if empty schema', () => {
+      const schema2 = string();
+      expect(schema2.pipe).toBeUndefined();
+    });
   });
 });
