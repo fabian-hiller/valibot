c40a5b9fa7044dde3b685fa9ecf177ba3093f184
Aris Kemper <aris.github@gmail.com>
2024-01-30 09:42:30 +0100

    docs: hexColor validation

---
+++ website/src/routes/api/(validations)/hexColor/properties.ts
@@ -0,0 +1,31 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TInput: {
+    type: 'string',
+  },
+  message: {
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+        },
+        'undefined',
+      ],
+    },
+    default: {
+      type: 'string',
+      value: 'Invalid hex color',
+    },
+  },
+  Validation: {
+    type: {
+      type: 'custom',
+      name: 'HexColorValidation',
+      href: '../HexColorValidation/',
+    },
+  },
+};
