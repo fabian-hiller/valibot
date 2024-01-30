8eb4cd315a5f2f83ca6b3f0341c3e2a1c0643987
Fabian Hiller <hillerfabian11@gmail.com>
2024-01-30 13:01:18 -0500

    Improve content of hash API reference on website

--- website/src/routes/api/(validations)/hash/properties.ts
+++ website/src/routes/api/(validations)/hash/properties.ts
@@ -6,68 +6,23 @@ export const properties: Record<string, PropertyProps> = {
   },
   types: {
     type: {
-      type: 'array',
-      item: {
-        type: 'union',
-        options: [
-          {
-            type: 'string',
-            value: 'md4',
+      type: 'tuple',
+      items: [
+        {
+          type: 'custom',
+          name: 'HashType',
+          href: '../HashType/',
+        },
+        {
+          type: 'array',
+          spread: true,
+          item: {
+            type: 'custom',
+            name: 'HashType',
+            href: '../HashType/',
           },
-          {
-            type: 'string',
-            value: 'md5',
-          },
-          {
-            type: 'string',
-            value: 'sha1',
-          },
-          {
-            type: 'string',
-            value: 'sha256',
-          },
-          {
-            type: 'string',
-            value: 'sha384',
-          },
-          {
-            type: 'string',
-            value: 'sha512',
-          },
-          {
-            type: 'string',
-            value: 'ripemd128',
-          },
-          {
-            type: 'string',
-            value: 'ripemd160',
-          },
-          {
-            type: 'string',
-            value: 'tiger128',
-          },
-          {
-            type: 'string',
-            value: 'tiger160',
-          },
-          {
-            type: 'string',
-            value: 'tiger192',
-          },
-          {
-            type: 'string',
-            value: 'crc32',
-          },
-          {
-            type: 'string',
-            value: 'crc32b',
-          },
-          {
-            type: 'string',
-            value: 'adler32',
-          },
-        ],
-      },
+        },
+      ],
     },
   },
   message: {
