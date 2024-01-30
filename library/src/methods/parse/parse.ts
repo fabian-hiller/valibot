52cc6bc92e528ec502f70e1fb7924dd5a2bfa3a8
Fabian Hiller <hillerfabian11@gmail.com>
2024-01-30 18:10:24 -0500

    Add content to parse API reference on website

--- library/src/methods/parse/parse.ts
+++ library/src/methods/parse/parse.ts
@@ -3,7 +3,7 @@ import { getGlobalConfig } from '../../storages/index.ts';
 import type { BaseSchema, Config, Output } from '../../types/index.ts';
 
 /**
- * Parses unknown input based on a schema.
+ * Parses an unknown input based on a schema.
  *
  * @param schema The schema to be used.
  * @param input The input to be parsed.
