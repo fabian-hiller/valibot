cab2ac8d664088afd8dca8645f764c49e0da2ef0
Aris Kemper <aris.github@gmail.com>
2024-02-02 09:31:37 +0100

    docs: mac, mac48, mac64 validation

---
+++ website/src/routes/api/(validations)/mac64/properties.ts
@@ -0,0 +1,32 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TInput: {
+    modifier: 'extends',
+    type: 'string',
+  },
+  message: {
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+        },
+        'undefined',
+      ],
+    },
+    default: {
+      type: 'string',
+      value: 'Invalid 64 bit MAC',
+    },
+  },
+  Validation: {
+    type: {
+      type: 'custom',
+      name: 'Mac64Validation',
+      href: '../Mac64Validation/',
+    },
+  },
+};
