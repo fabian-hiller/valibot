a59c823a6a1d2479d7c44d724ba6374cb4130fae
Aris Kemper <aris.github@gmail.com>
2024-02-02 09:31:37 +0100

    docs: mac, mac48, mac64 validation

---
+++ website/src/routes/api/(validations)/mac/properties.ts
@@ -0,0 +1,32 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TInput: {
+    modifier: 'extends',
+    type: 'string',
+  },
+  message: {
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+        },
+        'undefined',
+      ],
+    },
+    default: {
+      type: 'string',
+      value: 'Invalid MAC',
+    },
+  },
+  Validation: {
+    type: {
+      type: 'custom',
+      name: 'MacValidation',
+      href: '../MacValidation/',
+    },
+  },
+};
---
+++ website/src/routes/api/(validations)/mac48/properties.ts
@@ -0,0 +1,32 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TInput: {
+    modifier: 'extends',
+    type: 'string',
+  },
+  message: {
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+        },
+        'undefined',
+      ],
+    },
+    default: {
+      type: 'string',
+      value: 'Invalid 48 bit MAC',
+    },
+  },
+  Validation: {
+    type: {
+      type: 'custom',
+      name: 'Mac48Validation',
+      href: '../Mac48Validation/',
+    },
+  },
+};
