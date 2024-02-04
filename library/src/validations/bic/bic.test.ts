52bb5f3a55732712d7604def35880f2313c7cad7
Fabian Hiller <hillerfabian11@gmail.com>
2024-02-03 22:28:02 -0500

    Fix and improve unit tests of bic validation

--- library/src/validations/bic/bic.test.ts
+++ library/src/validations/bic/bic.test.ts
@@ -7,14 +7,14 @@ describe('bic', () => {
 
     const value1 = 'DEUTDEFF';
     expect(validate._parse(value1).output).toBe(value1);
-    const value2 = 'deutdeff';
-    expect(validate._parse(value2).output).toBe(value2);
     const value3 = 'DEUTDEFF400';
     expect(validate._parse(value3).output).toBe(value3);
     const value4 = 'NEDSZAJJXXX';
     expect(validate._parse(value4).output).toBe(value4);
 
     expect(validate._parse('').issues).toBeTruthy();
+    expect(validate._parse('deutdeff').issues).toBeTruthy();
+    expect(validate._parse('deutdeff400').issues).toBeTruthy();
     expect(validate._parse('DEUTFF').issues).toBeTruthy();
     expect(validate._parse('DEUT5EFF').issues).toBeTruthy();
     expect(validate._parse('DE1TDEFF').issues).toBeTruthy();
