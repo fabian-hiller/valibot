a2e6ca5ca165d41c4120a474ada9befe4367241e
Aris Kemper <aris.github@gmail.com>
2024-02-05 11:51:42 +0100

    feat: add min length for number

--- library/src/validations/minLength/minLength.test.ts
+++ library/src/validations/minLength/minLength.test.ts
@@ -2,20 +2,58 @@ import { describe, expect, test } from 'vitest';
 import { minLength } from './minLength.ts';
 
 describe('minLength', () => {
-  test('should pass only valid lengths', () => {
-    const validate = minLength(3);
+  describe('should pass', () => {
+    test('valid length of string numbers', () => {
+      const value1 = '123';
+      expect(minLength(3)._parse(value1).output).toBe(value1);
+    });
 
-    const value1 = '123';
-    expect(validate._parse(value1).output).toBe(value1);
-    const value2 = '123456789';
-    expect(validate._parse(value2).output).toBe(value2);
-    const value3 = [1, 2, 3];
-    expect(validate._parse(value3).output).toBe(value3);
+    test('valid length of numbers', () => {
+      const value1 = 123;
+      expect(minLength(3)._parse(value1).output).toBe(value1);
+    });
 
-    expect(validate._parse('').issues).toBeTruthy();
-    expect(validate._parse('12').issues).toBeTruthy();
-    expect(validate._parse([]).issues).toBeTruthy();
-    expect(validate._parse([1, 2]).issues).toBeTruthy();
+    test('valid length of numbers higher then min length', () => {
+      const value1 = 12356;
+      expect(minLength(3)._parse(value1).output).toBe(value1);
+    });
+
+    test('valid length of string numbers higher then min length', () => {
+      const value1 = '12356';
+      expect(minLength(3)._parse(value1).output).toBe(value1);
+    });
+
+    test('valid length of array of numbers', () => {
+      const value1 = [1, 2, 3];
+      expect(minLength(3)._parse(value1).output).toBe(value1);
+    });
+
+    test('valid length of array of empty objects', () => {
+      const value1 = [{}, {}, {}];
+      expect(minLength(3)._parse(value1).output).toBe(value1);
+    });
+  });
+
+  describe('should reject', () => {
+    test('empty string', () => {
+      expect(minLength(3)._parse('').issues).toBeTruthy();
+    });
+
+    test('string with invalid length', () => {
+      expect(minLength(3)._parse('12').issues).toBeTruthy();
+    });
+
+    test('empty array', () => {
+      expect(minLength(3)._parse([]).issues).toBeTruthy();
+    });
+
+    test('array of numbers with length lower then min length', () => {
+      expect(minLength(3)._parse([1, 2]).issues).toBeTruthy();
+    });
+
+    test('array of objects with length lower then min length', () => {
+      expect(minLength(3)._parse([{}, {}]).issues).toBeTruthy();
+    });
   });
 
   test('should return custom error message', () => {
