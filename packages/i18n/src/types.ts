8ec57ab480a0197479b72a0044d4e571be1ddf20
Max Levytskyi <immaxlev@gmail.com>
2024-02-06 20:48:38 +0200

    feat (i18n): add ukrainian language

--- packages/i18n/src/types.ts
+++ packages/i18n/src/types.ts
@@ -1,7 +1,7 @@
 import type { ErrorMessage } from 'valibot';
 
 export type Language = {
-  code: 'de' | 'en';
+  code: 'de' | 'en' | 'uk';
   schema: ErrorMessage;
   specific: {
     bic: ErrorMessage;
