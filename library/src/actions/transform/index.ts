e815db8df10c855aba376ca5d595d2bf2dd9cae2
Fabian Hiller <hillerfabian11@gmail.com>
2024-04-12 13:32:37 -0400

    Add asyc implementation to transform action

--- library/src/actions/transform/index.ts
+++ library/src/actions/transform/index.ts
@@ -1,1 +1,2 @@
 export * from './transform.ts';
+export * from './transformAsync.ts';
---
+++ library/src/actions/transform/transformAsync.ts
@@ -0,0 +1,43 @@
+import type {
+  BaseTransformationAsync,
+  TypedDataset,
+} from '../../types/index.ts';
+
+/**
+ * Transform action async type.
+ */
+export interface TransformActionAsync<TInput, TOutput>
+  extends BaseTransformationAsync<TInput, TOutput, never> {
+  /**
+   * The action type.
+   */
+  readonly type: 'transform';
+  /**
+   * The transformation action.
+   */
+  readonly action: (input: TInput) => Promise<TOutput>;
+}
+
+/**
+ * Creates a custom transformation action.
+ *
+ * @param action The transformation logic.
+ *
+ * @returns A transform action.
+ */
+export function transformAsync<TInput, TOutput>(
+  action: (input: TInput) => Promise<TOutput>
+): TransformActionAsync<TInput, TOutput> {
+  return {
+    kind: 'transformation',
+    type: 'transform',
+    action,
+    async: true,
+    async _run(dataset) {
+      // @ts-expect-error
+      dataset.value = await action(dataset.value);
+      // @ts-expect-error
+      return dataset as TypedDataset<TOutput, never>;
+    },
+  };
+}
