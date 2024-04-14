e51cd52a8d566b73920be1bb0e4d7319da4d7f74
Afsoon <said.atrahouch@gmail.com>
2024-04-14 11:08:53 +0200

    cuid2 validation

---
+++ library/src/actions/cuid2/cuid2.test.ts
@@ -0,0 +1,83 @@
+import { describe, expect, test } from 'vitest';
+import { expectActionIssue, expectNoActionIssue } from '../../vitest/index.ts';
+import { cuid2, type Cuid2Action, type Cuid2Issue } from './cuid2.ts';
+
+describe('cuid2', () => {
+  describe('should return action object', () => {
+    const baseAction: Omit<Cuid2Action<string, never>, 'message'> = {
+      kind: 'validation',
+      type: 'credit_card',
+      expects: null,
+      requirement: expect.any(Function),
+      async: false,
+      _run: expect.any(Function),
+    };
+
+    test('with undefined message', () => {
+      const action: Cuid2Action<string, undefined> = {
+        ...baseAction,
+        message: undefined,
+      };
+      expect(cuid2()).toStrictEqual(action);
+      expect(cuid2(undefined)).toStrictEqual(action);
+    });
+
+    test('with string message', () => {
+      expect(cuid2('message')).toStrictEqual({
+        ...baseAction,
+        message: 'message',
+      } satisfies Cuid2Action<string, string>);
+    });
+
+    test('with function message', () => {
+      const message = () => 'message';
+      expect(cuid2(message)).toStrictEqual({
+        ...baseAction,
+        message,
+      } satisfies Cuid2Action<string, typeof message>);
+    });
+  });
+
+  describe('should return dataset without issues', () => {
+    const action = cuid2();
+
+    test('for untyped inputs', () => {
+      expect(action._run({ typed: false, value: null }, {})).toStrictEqual({
+        typed: false,
+        value: null,
+      });
+    });
+
+    test('for valid cuid2', () => {
+      const validCuid2s = [
+        'o2dyrckf0vbqhftbcx8ex7r8',
+        'pj17j4wheabtydu00x2yuo8s',
+        'vkydd2qpoediyioixyeh8zyo',
+        'ja3j1arc87i80ys1zxk8iyiv',
+        'pbe6zw7wikj83vv5knjk1wx8',
+      ];
+      expectNoActionIssue(action, validCuid2s);
+    });
+  });
+
+  describe('should return dataset with issues', () => {
+    const action = cuid2('message');
+    const baseIssue: Omit<Cuid2Issue<string>, 'input' | 'received'> = {
+      kind: 'validation',
+      type: 'credit_card',
+      expected: null,
+      message: 'message',
+      requirement: expect.any(Function),
+    };
+
+    test('for invalid cuid2', () => {
+      expectActionIssue(action, baseIssue, [
+        '',
+        'w#@%^',
+        'o2dyrcKf0vbqhftBcx8ex7r8',
+        '1vx6pa5rqog2tqdztxaa0xgw',
+        'Not a CUID2',
+      ]);
+    });
+  });
+});
---
+++ library/src/actions/cuid2/cuid2.ts
@@ -0,0 +1,102 @@
+import { CUID2_REGEX } from '../../regex.ts';
+import type {
+  BaseIssue,
+  BaseValidation,
+  ErrorMessage,
+} from '../../types/index.ts';
+import { _validationDataset } from '../../utils/index.ts';
+
+/**
+ * Cuid2 issue type.
+ */
+export interface Cuid2Issue<TInput extends string> extends BaseIssue<TInput> {
+  /**
+   * The issue kind.
+   */
+  readonly kind: 'validation';
+  /**
+   * The issue type.
+   */
+  readonly type: 'credit_card';
+  /**
+   * The expected input.
+   */
+  readonly expected: null;
+  /**
+   * The received input.
+   */
+  readonly received: `${number}`;
+  /**
+   * The minimum bytes.
+   */
+  readonly requirement: (input: string) => boolean;
+}
+
+/**
+ * Cuid2 action type.
+ */
+export interface Cuid2Action<
+  TInput extends string,
+  TMessage extends ErrorMessage<Cuid2Issue<TInput>> | undefined,
+> extends BaseValidation<TInput, TInput, Cuid2Issue<TInput>> {
+  /**
+   * The action type.
+   */
+  readonly type: 'credit_card';
+  /**
+   * The expected property.
+   */
+  readonly expects: null;
+  /**
+   * The minimum bytes.
+   */
+  readonly requirement: (input: string) => boolean;
+  /**
+   * The error message.
+   */
+  readonly message: TMessage;
+}
+
+/**
+ * Creates a Cuid2 validation action.
+ *
+ * @returns A Cuid2 action.
+ */
+export function cuid2<TInput extends string>(): Cuid2Action<TInput, undefined>;
+
+/**
+ * Creates a Cuid2 validation action.
+ *
+ * @param message The error message.
+ *
+ * @returns A Cuid2 action.
+ */
+export function cuid2<
+  TInput extends string,
+  const TMessage extends ErrorMessage<Cuid2Issue<TInput>> | undefined,
+>(message: TMessage): Cuid2Action<TInput, TMessage>;
+
+export function cuid2(
+  message?: ErrorMessage<Cuid2Issue<string>>
+): Cuid2Action<string, ErrorMessage<Cuid2Issue<string>> | undefined> {
+  return {
+    kind: 'validation',
+    type: 'credit_card',
+    expects: null,
+    async: false,
+    message,
+    requirement(input) {
+      return CUID2_REGEX.test(input);
+    },
+    _run(dataset, config) {
+      return _validationDataset(
+        this,
+        cuid2,
+        'credit_card',
+        dataset.typed && !this.requirement(dataset.value),
+        dataset,
+        config
+      );
+    },
+  };
+}
