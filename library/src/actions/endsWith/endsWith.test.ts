c6c9eec43035b6f0267f69de7a75e9945751a1b3
Elton Lobo <eltonsw00@gmail.com>
2024-04-27 22:51:28 +0530

    ✨ feat: add endsWith action

---
+++ library/src/actions/endsWith/endsWith.test.ts
@@ -0,0 +1,83 @@
+import { describe, expect, test } from 'vitest';
+import { expectActionIssue, expectNoActionIssue } from '../../vitest/index.ts';
+import {
+  endsWith,
+  type EndsWithAction,
+  type EndsWithIssue,
+} from './endsWith.ts';
+
+describe('endsWith', () => {
+  describe('should return action object', () => {
+    const baseAction: Omit<EndsWithAction<string, 'abc', never>, 'message'> = {
+      kind: 'validation',
+      type: 'ends_with',
+      reference: endsWith,
+      expects: '"abc"',
+      requirement: 'abc',
+      async: false,
+      _run: expect.any(Function),
+    };
+
+    test('with undefined message', () => {
+      const action: EndsWithAction<string, 'abc', undefined> = {
+        ...baseAction,
+        message: undefined,
+      };
+      expect(endsWith('abc')).toStrictEqual(action);
+      expect(endsWith('abc', undefined)).toStrictEqual(action);
+    });
+
+    test('with string message', () => {
+      expect(endsWith('abc', 'message')).toStrictEqual({
+        ...baseAction,
+        message: 'message',
+      } satisfies EndsWithAction<string, 'abc', string>);
+    });
+
+    test('with function message', () => {
+      const message = () => 'message';
+      expect(endsWith('abc', message)).toStrictEqual({
+        ...baseAction,
+        message,
+      } satisfies EndsWithAction<string, 'abc', typeof message>);
+    });
+  });
+
+  describe('should return dataset without issues', () => {
+    const action = endsWith('abc');
+
+    test('for untyped inputs', () => {
+      expect(action._run({ typed: false, value: null }, {})).toStrictEqual({
+        typed: false,
+        value: null,
+      });
+    });
+
+    test('for valid inputs', () => {
+      expectNoActionIssue(action, ['abc', '123abc', 'xyzabc', 'xyz123abc']);
+    });
+  });
+
+  describe('should return dataset with issues', () => {
+    const action = endsWith('abc', 'message');
+    const baseIssue: Omit<
+      EndsWithIssue<string, 'abc'>,
+      'input' | 'received'
+    > = {
+      kind: 'validation',
+      type: 'ends_with',
+      expected: '"abc"',
+      message: 'message',
+      requirement: 'abc',
+    };
+
+    test('for invalid inputs', () => {
+      expectActionIssue(
+        action,
+        baseIssue,
+        ['', 'abc ', '123a', '123ab', 'xyzab', 'abcc', 'abcz', 'zabcdef'],
+        (value) => `"${value.slice(-'abc'.length)}"`
+      );
+    });
+  });
+});
---
+++ library/src/actions/endsWith/endsWith.ts
@@ -0,0 +1,125 @@
+import type {
+  BaseIssue,
+  BaseValidation,
+  ErrorMessage,
+} from '../../types/index.ts';
+import { _addIssue } from '../../utils/index.ts';
+
+/**
+ * Ends with issue type.
+ */
+export interface EndsWithIssue<
+  TInput extends string,
+  TRequirement extends string,
+> extends BaseIssue<TInput> {
+  /**
+   * The issue kind.
+   */
+  readonly kind: 'validation';
+  /**
+   * The issue type.
+   */
+  readonly type: 'ends_with';
+  /**
+   * The expected input.
+   */
+  readonly expected: `"${TRequirement}"`;
+  /**
+   * The received input.
+   */
+  readonly received: `"${string}"`;
+  /**
+   * The end string.
+   */
+  readonly requirement: TRequirement;
+}
+
+/**
+ * Ends with action type.
+ */
+export interface EndsWithAction<
+  TInput extends string,
+  TRequirement extends string,
+  TMessage extends
+    | ErrorMessage<EndsWithIssue<TInput, TRequirement>>
+    | undefined,
+> extends BaseValidation<TInput, TInput, EndsWithIssue<TInput, TRequirement>> {
+  /**
+   * The action type.
+   */
+  readonly type: 'ends_with';
+  /**
+   * The action reference.
+   */
+  readonly reference: typeof endsWith;
+  /**
+   * The expected property.
+   */
+  readonly expects: `"${TRequirement}"`;
+  /**
+   * The end string.
+   */
+  readonly requirement: TRequirement;
+  /**
+   * The error message.
+   */
+  readonly message: TMessage;
+}
+
+/**
+ * Creates an ends with validation action.
+ *
+ * @param requirement The end string.
+ *
+ * @returns An ends with action.
+ */
+export function endsWith<
+  TInput extends string,
+  const TRequirement extends string,
+>(requirement: TRequirement): EndsWithAction<TInput, TRequirement, undefined>;
+
+/**
+ * Creates an ends with validation action.
+ *
+ * @param requirement The end string.
+ * @param message The error message.
+ *
+ * @returns An ends with action.
+ */
+export function endsWith<
+  TInput extends string,
+  const TRequirement extends string,
+  const TMessage extends
+    | ErrorMessage<EndsWithIssue<TInput, TRequirement>>
+    | undefined,
+>(
+  requirement: TRequirement,
+  message: TMessage
+): EndsWithAction<TInput, TRequirement, TMessage>;
+
+export function endsWith(
+  requirement: string,
+  message?: ErrorMessage<EndsWithIssue<string, string>>
+): EndsWithAction<
+  string,
+  string,
+  ErrorMessage<EndsWithIssue<string, string>> | undefined
+> {
+  return {
+    kind: 'validation',
+    type: 'ends_with',
+    reference: endsWith,
+    async: false,
+    expects: `"${requirement}"`,
+    requirement,
+    message,
+    _run(dataset, config) {
+      if (dataset.typed && !dataset.value.endsWith(this.requirement)) {
+        _addIssue(this, 'end', dataset, config, {
+          received: `"${dataset.value.slice(-this.requirement.length)}"`,
+        });
+      }
+      return dataset;
+    },
+  };
+}
