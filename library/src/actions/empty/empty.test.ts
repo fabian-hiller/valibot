19686ccd1e2c834ac5f0ab66539cf34b1327aabb
Fabian Hiller <hillerfabian11@gmail.com>
2024-05-18 19:57:55 -0400

    Add empty and nonEmpty action with unit and type tests

---
+++ library/src/actions/empty/empty.test.ts
@@ -0,0 +1,87 @@
+import { describe, expect, test } from 'vitest';
+import { expectActionIssue, expectNoActionIssue } from '../../vitest/index.ts';
+import { empty, type EmptyAction, type EmptyIssue } from './empty.ts';
+
+describe('empty', () => {
+  describe('should return action object', () => {
+    const baseAction: Omit<EmptyAction<string, never>, 'message'> = {
+      kind: 'validation',
+      type: 'empty',
+      reference: empty,
+      expects: '0',
+      async: false,
+      _run: expect.any(Function),
+    };
+
+    test('with undefined message', () => {
+      const action: EmptyAction<string, undefined> = {
+        ...baseAction,
+        message: undefined,
+      };
+      expect(empty()).toStrictEqual(action);
+      expect(empty(undefined)).toStrictEqual(action);
+    });
+
+    test('with string message', () => {
+      expect(empty('message')).toStrictEqual({
+        ...baseAction,
+        message: 'message',
+      } satisfies EmptyAction<string, string>);
+    });
+
+    test('with function message', () => {
+      const message = () => 'message';
+      expect(empty(message)).toStrictEqual({
+        ...baseAction,
+        message,
+      } satisfies EmptyAction<string, typeof message>);
+    });
+  });
+
+  describe('should return dataset without issues', () => {
+    const action = empty();
+
+    test('for untyped inputs', () => {
+      expect(action._run({ typed: false, value: null }, {})).toStrictEqual({
+        typed: false,
+        value: null,
+      });
+    });
+
+    test('for valid strings', () => {
+      expectNoActionIssue(action, ['']);
+    });
+
+    test('for valid arrays', () => {
+      expectNoActionIssue(action, [[]]);
+    });
+  });
+
+  describe('should return dataset with issues', () => {
+    const action = empty('message');
+    const baseIssue: Omit<EmptyIssue<string>, 'input' | 'received'> = {
+      kind: 'validation',
+      type: 'empty',
+      expected: '0',
+      message: 'message',
+    };
+
+    test('for invalid strings', () => {
+      expectActionIssue(
+        action,
+        baseIssue,
+        [' ', '\n', 'foo', 'foobarbaz123'],
+        (input) => `${input.length}`
+      );
+    });
+
+    test('for invalid arrays', () => {
+      expectActionIssue(
+        action,
+        baseIssue,
+        [[null], [1, 2, 3, 4, 6], Array(999)],
+        (input) => `${input.length}`
+      );
+    });
+  });
+});
---
+++ library/src/actions/empty/empty.ts
@@ -0,0 +1,98 @@
+import type {
+  BaseIssue,
+  BaseValidation,
+  ErrorMessage,
+} from '../../types/index.ts';
+import { _addIssue } from '../../utils/index.ts';
+import type { LengthInput } from '../types.ts';
+
+/**
+ * Empty issue type.
+ */
+export interface EmptyIssue<TInput extends LengthInput>
+  extends BaseIssue<TInput> {
+  /**
+   * The issue kind.
+   */
+  readonly kind: 'validation';
+  /**
+   * The issue type.
+   */
+  readonly type: 'empty';
+  /**
+   * The expected input.
+   */
+  readonly expected: '0';
+  /**
+   * The received input.
+   */
+  readonly received: `${number}`;
+}
+
+/**
+ * Empty action type.
+ */
+export interface EmptyAction<
+  TInput extends LengthInput,
+  TMessage extends ErrorMessage<EmptyIssue<TInput>> | undefined,
+> extends BaseValidation<TInput, TInput, EmptyIssue<TInput>> {
+  /**
+   * The action type.
+   */
+  readonly type: 'empty';
+  /**
+   * The action reference.
+   */
+  readonly reference: typeof empty;
+  /**
+   * The expected property.
+   */
+  readonly expects: '0';
+  /**
+   * The error message.
+   */
+  readonly message: TMessage;
+}
+
+/**
+ * Creates an empty validation action.
+ *
+ * @returns An empty action.
+ */
+export function empty<TInput extends LengthInput>(): EmptyAction<
+  TInput,
+  undefined
+>;
+
+/**
+ * Creates an empty validation action.
+ *
+ * @param message The error message.
+ *
+ * @returns An empty action.
+ */
+export function empty<
+  TInput extends LengthInput,
+  const TMessage extends ErrorMessage<EmptyIssue<TInput>> | undefined,
+>(message: TMessage): EmptyAction<TInput, TMessage>;
+
+export function empty(
+  message?: ErrorMessage<EmptyIssue<LengthInput>>
+): EmptyAction<LengthInput, ErrorMessage<EmptyIssue<LengthInput>> | undefined> {
+  return {
+    kind: 'validation',
+    type: 'empty',
+    reference: empty,
+    async: false,
+    expects: '0',
+    message,
+    _run(dataset, config) {
+      if (dataset.typed && dataset.value.length > 0) {
+        _addIssue(this, 'length', dataset, config, {
+          received: `${dataset.value.length}`,
+        });
+      }
+      return dataset;
+    },
+  };
+}
