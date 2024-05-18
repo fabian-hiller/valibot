c7cf2f923db1a9a5a094f384023e53378088f170
Andrey Yolkin <andreyyolkin@gmail.com>
2024-05-18 16:10:09 +0300

    feat: `notSize` action

---
+++ library/src/actions/notSize/notSize.test-d.ts
@@ -0,0 +1,45 @@
+import { describe, expectTypeOf, test } from 'vitest';
+import type { InferInput, InferIssue, InferOutput } from '../../types/index.ts';
+import { notSize, type NotSizeAction, type NotSizeIssue } from './notSize.ts';
+
+describe('size', () => {
+  describe('should return action object', () => {
+    test('with undefined message', () => {
+      type Action = NotSizeAction<Blob, 10, undefined>;
+      expectTypeOf(notSize<Blob, 10>(10)).toEqualTypeOf<Action>();
+      expectTypeOf(
+        notSize<Blob, 10, undefined>(10, undefined)
+      ).toEqualTypeOf<Action>();
+    });
+
+    test('with string message', () => {
+      expectTypeOf(notSize<Blob, 10, 'message'>(10, 'message')).toEqualTypeOf<
+        NotSizeAction<Blob, 10, 'message'>
+      >();
+    });
+
+    test('with function message', () => {
+      expectTypeOf(
+        notSize<Blob, 10, () => string>(10, () => 'message')
+      ).toEqualTypeOf<NotSizeAction<Blob, 10, () => string>>();
+    });
+  });
+
+  describe('should infer correct types', () => {
+    type Action = NotSizeAction<Map<string, number>, 10, undefined>;
+
+    test('of input', () => {
+      expectTypeOf<InferInput<Action>>().toEqualTypeOf<Map<string, number>>();
+    });
+
+    test('of output', () => {
+      expectTypeOf<InferOutput<Action>>().toEqualTypeOf<Map<string, number>>();
+    });
+
+    test('of issue', () => {
+      expectTypeOf<InferIssue<Action>>().toEqualTypeOf<
+      NotSizeIssue<Map<string, number>, 10>
+      >();
+    });
+  });
+});
---
+++ library/src/actions/notSize/notSize.ts
@@ -0,0 +1,124 @@
+import type {
+  BaseIssue,
+  BaseValidation,
+  ErrorMessage,
+} from '../../types/index.ts';
+import { _addIssue } from '../../utils/index.ts';
+import type { SizeInput } from '../types.ts';
+
+/**
+ * Size issue type.
+ */
+export interface NotSizeIssue<
+  TInput extends SizeInput,
+  TRequirement extends number,
+> extends BaseIssue<TInput> {
+  /**
+   * The issue kind.
+   */
+  readonly kind: 'validation';
+  /**
+   * The issue type.
+   */
+  readonly type: 'not_size';
+  /**
+   * The expected input.
+   */
+  readonly expected: `!${TRequirement}`;
+  /**
+   * The received input.
+   */
+  readonly received: `${number}`;
+  /**
+   * The required size.
+   */
+  readonly requirement: TRequirement;
+}
+
+/**
+ * Size action type.
+ */
+export interface NotSizeAction<
+  TInput extends SizeInput,
+  TRequirement extends number,
+  TMessage extends ErrorMessage<NotSizeIssue<TInput, TRequirement>> | undefined,
+> extends BaseValidation<TInput, TInput, NotSizeIssue<TInput, TRequirement>> {
+  /**
+   * The action type.
+   */
+  readonly type: 'not_size';
+  /**
+   * The action reference.
+   */
+  readonly reference: typeof notSize;
+  /**
+   * The expected property.
+   */
+  readonly expects: `!${TRequirement}`;
+  /**
+   * The required size.
+   */
+  readonly requirement: TRequirement;
+  /**
+   * The error message.
+   */
+  readonly message: TMessage;
+}
+
+/**
+ * Creates a size validation action.
+ *
+ * @param requirement The required size.
+ *
+ * @returns A size action.
+ */
+export function notSize<
+  TInput extends SizeInput,
+  const TRequirement extends number,
+>(requirement: TRequirement): NotSizeAction<TInput, TRequirement, undefined>;
+
+/**
+ * Creates a size validation action.
+ *
+ * @param requirement The required size.
+ * @param message The error message.
+ *
+ * @returns A size action.
+ */
+export function notSize<
+  TInput extends SizeInput,
+  const TRequirement extends number,
+  const TMessage extends
+    | ErrorMessage<NotSizeIssue<TInput, TRequirement>>
+    | undefined,
+>(
+  requirement: TRequirement,
+  message: TMessage
+): NotSizeAction<TInput, TRequirement, TMessage>;
+
+export function notSize(
+  requirement: number,
+  message?: ErrorMessage<NotSizeIssue<SizeInput, number>>
+): NotSizeAction<
+  SizeInput,
+  number,
+  ErrorMessage<NotSizeIssue<SizeInput, number>> | undefined
+> {
+  return {
+    kind: 'validation',
+    type: 'not_size',
+    reference: notSize,
+    async: false,
+    expects: `!${requirement}`,
+    requirement,
+    message,
+    _run(dataset, config) {
+      if (dataset.typed && dataset.value.size === this.requirement) {
+        _addIssue(this, 'size', dataset, config, {
+          received: `${dataset.value.size}`,
+        });
+      }
+      return dataset;
+    },
+  };
+}
