2d9efe50ec35b40ffdd43ba18d6ea6ca2f7bbb8d
Fabian Hiller <hillerfabian11@gmail.com>
2024-05-20 12:42:36 -0400

    Add looseTupleAsync schema with unit and type tests

--- library/src/schemas/looseTuple/looseTupleAsync.ts
+++ library/src/schemas/looseTuple/looseTupleAsync.ts
@@ -1,11 +1,15 @@
 import type {
+  BaseIssue,
   BaseSchemaAsync,
+  Dataset,
   ErrorMessage,
   InferTupleInput,
   InferTupleIssue,
   InferTupleOutput,
   TupleItemsAsync,
+  TuplePathItem,
 } from '../../types/index.ts';
+import { _addIssue } from '../../utils/index.ts';
 import type { LooseTupleIssue } from './types.ts';
 
 /**
@@ -23,10 +27,10 @@ export interface LooseTupleSchemaAsync<
    * The schema type.
    */
   readonly type: 'loose_tuple';
-  // /**
-  //  * The schema reference.
-  //  */
-  // readonly reference: typeof looseTupleAsync;
+  /**
+   * The schema reference.
+   */
+  readonly reference: typeof looseTupleAsync;
   /**
    * The expected property.
    */
@@ -39,4 +43,133 @@ export interface LooseTupleSchemaAsync<
    * The error message.
    */
   readonly message: TMessage;
+}
+
+/**
+ * Creates a loose tuple schema.
+ *
+ * @param items The items schema.
+ *
+ * @returns A loose tuple schema.
+ */
+export function looseTupleAsync<const TItems extends TupleItemsAsync>(
+  items: TItems
+): LooseTupleSchemaAsync<TItems, undefined>;
+
+/**
+ * Creates a loose tuple schema.
+ *
+ * @param items The items schema.
+ * @param message The error message.
+ *
+ * @returns A loose tuple schema.
+ */
+export function looseTupleAsync<
+  const TItems extends TupleItemsAsync,
+  const TMessage extends ErrorMessage<LooseTupleIssue> | undefined,
+>(items: TItems, message: TMessage): LooseTupleSchemaAsync<TItems, TMessage>;
+
+export function looseTupleAsync(
+  items: TupleItemsAsync,
+  message?: ErrorMessage<LooseTupleIssue>
+): LooseTupleSchemaAsync<
+  TupleItemsAsync,
+  ErrorMessage<LooseTupleIssue> | undefined
+> {
+  return {
+    kind: 'schema',
+    type: 'loose_tuple',
+    reference: looseTupleAsync,
+    expects: 'Array',
+    async: true,
+    items,
+    message,
+    async _run(dataset, config) {
+      // Get input value from dataset
+      const input = dataset.value;
+
+      // If root type is valid, check nested types
+      if (Array.isArray(input)) {
+        // Set typed to `true` and value to empty array
+        dataset.typed = true;
+        dataset.value = [];
+
+        // Parse schema of each tuple item
+        const itemDatasets = await Promise.all(
+          items.map(async (item, key) => {
+            const value = input[key];
+            return [
+              key,
+              value,
+              await item._run({ typed: false, value }, config),
+            ] as const;
+          })
+        );
+
+        // Process each tuple item dataset
+        for (const [key, value, itemDataset] of itemDatasets) {
+          // If there are issues, capture them
+          if (itemDataset.issues) {
+            // Create tuple path item
+            const pathItem: TuplePathItem = {
+              type: 'tuple',
+              origin: 'value',
+              input,
+              key,
+              value,
+            };
+
+            // Add modified item dataset issues to issues
+            for (const issue of itemDataset.issues) {
+              if (issue.path) {
+                issue.path.unshift(pathItem);
+              } else {
+                // @ts-expect-error
+                issue.path = [pathItem];
+              }
+              // @ts-expect-error
+              dataset.issues?.push(issue);
+            }
+            if (!dataset.issues) {
+              // @ts-expect-error
+              dataset.issues = itemDataset.issues;
+            }
+
+            // If necessary, abort early
+            if (config.abortEarly) {
+              dataset.typed = false;
+              break;
+            }
+          }
+
+          // If not typed, set typed to `false`
+          if (!itemDataset.typed) {
+            dataset.typed = false;
+          }
+
+          // Add item to dataset
+          // @ts-expect-error
+          dataset.value.push(itemDataset.value);
+        }
+
+        // Add rest to dataset if necessary
+        if (!dataset.issues || !config.abortEarly) {
+          for (let key = items.length; key < input.length; key++) {
+            // @ts-expect-error
+            dataset.value.push(input[key]);
+          }
+        }
+
+        // Otherwise, add tuple issue
+      } else {
+        _addIssue(this, 'type', dataset, config);
+      }
+
+      // Return output dataset
+      return dataset as Dataset<
+        unknown[],
+        LooseTupleIssue | BaseIssue<unknown>
+      >;
+    },
+  };
 }
