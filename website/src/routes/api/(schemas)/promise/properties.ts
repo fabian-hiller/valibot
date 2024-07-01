3109310f7981fd8f0c2d75843fe31d42500bfcef
Fabian Hiller <hillerfabian11@gmail.com>
2024-07-01 22:20:16 +0200

    Add content to file, function and promise API reference

---
+++ website/src/routes/api/(schemas)/promise/properties.ts
@@ -0,0 +1,44 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TMessage: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'PromiseIssue',
+              href: '../PromiseIssue/',
+            },
+          ],
+        },
+        'undefined',
+      ],
+    },
+  },
+  message: {
+    type: {
+      type: 'custom',
+      name: 'TMessage',
+    },
+  },
+  Schema: {
+    type: {
+      type: 'custom',
+      name: 'PromiseSchema',
+      href: '../PromiseSchema/',
+      generics: [
+        {
+          type: 'custom',
+          name: 'TMessage',
+        },
+      ],
+    },
+  },
+};
