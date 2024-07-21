4bf68c27ecf7d9e7f2298296cc6a415b15514b3d
Fabian Hiller <hillerfabian11@gmail.com>
2024-07-21 20:22:34 +0200

    Change unit tests of fallbackAsync to match other tests

--- library/src/methods/fallback/fallbackAsync.test.ts
+++ library/src/methods/fallback/fallbackAsync.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from 'vitest';
-import { checkAsync, transform } from '../../actions/index.ts';
-import { number, string } from '../../schemas/index.ts';
-import { pipe, pipeAsync } from '../pipe/index.ts';
+import { transformAsync } from '../../actions/index.ts';
+import { number } from '../../schemas/index.ts';
+import { pipeAsync } from '../pipe/index.ts';
 import {
   fallbackAsync,
   type SchemaWithFallbackAsync,
@@ -9,7 +9,10 @@ import {
 
 describe('fallbackAsync', () => {
   describe('should return schema object', () => {
-    const schema = pipe(number(), transform(String));
+    const schema = pipeAsync(
+      number(),
+      transformAsync(async (input) => String(input))
+    );
     type Schema = typeof schema;
     const baseSchema: Omit<
       SchemaWithFallbackAsync<Schema, never>,
@@ -44,63 +47,32 @@ describe('fallbackAsync', () => {
     });
   });
 
-  describe('when sync schema passed as the argument', () => {
-    const schema = fallbackAsync(
-      pipe(number(), transform(String)),
-      async () => '123'
-    );
+  const schema = fallbackAsync(
+    pipeAsync(
+      number(),
+      transformAsync(async (input) => String(input))
+    ),
+    async () => '123'
+  );
 
-    describe('should return default dataset', () => {
-      test('for valid input', async () => {
-        expect(
-          await schema._run({ typed: false, value: 789 }, {})
-        ).toStrictEqual({
+  describe('should return default dataset', () => {
+    test('for valid input', async () => {
+      expect(await schema._run({ typed: false, value: 789 }, {})).toStrictEqual(
+        {
           typed: true,
           value: '789',
-        });
-      });
-    });
-
-    describe('should return dataset with fallback', () => {
-      test('for invalid input', async () => {
-        expect(
-          await schema._run({ typed: false, value: 'foo' }, {})
-        ).toStrictEqual({
-          typed: true,
-          value: '123',
-        });
-      });
+        }
+      );
     });
   });
 
-  describe('when async schema passed as the argument', () => {
-    const schema = fallbackAsync(
-      pipeAsync(
-        string(),
-        checkAsync(async (value) => value === 'abcde')
-      ),
-      async () => '12345'
-    );
-
-    describe('should return default dataset', () => {
-      test('for valid input', async () => {
-        expect(
-          await schema._run({ typed: false, value: 'abcde' }, {})
-        ).toStrictEqual({
-          typed: true,
-          value: 'abcde',
-        });
-      });
-    });
-
-    describe('should return dataset with fallback', () => {
-      test('for invalid input', async () => {
-        expect(
-          await schema._run({ typed: false, value: 'abc' }, {})
-        ).toStrictEqual({
-          typed: true,
-          value: '12345',
-        });
+  describe('should return dataset with fallback', () => {
+    test('for invalid input', async () => {
+      expect(
+        await schema._run({ typed: false, value: 'foo' }, {})
+      ).toStrictEqual({
+        typed: true,
+        value: '123',
       });
     });
   });
