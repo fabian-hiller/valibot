2f8875083dd92c5ad2ddaa3a4c43cb70eba38eb7
mehm8128 <mehm8128@gmail.com>
2024-07-24 01:52:28 +0900

    add looseTupleAsync doc

---
+++ website/src/routes/api/(types)/LooseTupleSchemaAsync/properties.ts
@@ -0,0 +1,140 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TItems: {
+    modifier: 'extends',
+    type: {
+      type: 'custom',
+      name: 'TupleItemsAsync',
+      href: '../TupleItemsAsync/',
+    },
+  },
+  TMessage: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'LooseTupleIssue',
+              href: '../LooseTupleIssue/',
+            },
+          ],
+        },
+        'undefined',
+      ],
+    },
+  },
+  BaseSchemaAsync: {
+    modifier: 'extends',
+    type: {
+      type: 'custom',
+      name: 'BaseSchemaAsync',
+      href: '../BaseSchemaAsync/',
+      generics: [
+        {
+          type: 'tuple',
+          items: [
+            {
+              type: 'custom',
+              spread: true,
+              name: 'InferTupleInput',
+              href: '../InferTupleInput/',
+              generics: [
+                {
+                  type: 'custom',
+                  name: 'TItems',
+                },
+              ],
+            },
+            {
+              type: 'array',
+              spread: true,
+              item: 'unknown',
+            },
+          ],
+        },
+        {
+          type: 'tuple',
+          items: [
+            {
+              type: 'custom',
+              spread: true,
+              name: 'InferTupleOutput',
+              href: '../InferTupleOutput/',
+              generics: [
+                {
+                  type: 'custom',
+                  name: 'TItems',
+                },
+              ],
+            },
+            {
+              type: 'array',
+              spread: true,
+              item: 'unknown',
+            },
+          ],
+        },
+        {
+          type: 'union',
+          options: [
+            {
+              type: 'custom',
+              name: 'LooseTupleIssue',
+              href: '../LooseTupleIssue/',
+            },
+            {
+              type: 'custom',
+              name: 'InferTupleIssue',
+              href: '../InferTupleIssue/',
+              generics: [
+                {
+                  type: 'custom',
+                  name: 'TItems',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  type: {
+    type: {
+      type: 'string',
+      value: 'loose_tuple',
+    },
+  },
+  reference: {
+    type: {
+      type: 'custom',
+      modifier: 'typeof',
+      name: 'looseTupleAsync',
+      href: '../looseTupleAsync/',
+    },
+  },
+  expects: {
+    type: {
+      type: 'string',
+      value: 'Array',
+    },
+  },
+  items: {
+    type: {
+      type: 'custom',
+      name: 'TItems',
+    },
+  },
+  message: {
+    type: {
+      type: 'custom',
+      name: 'TMessage',
+    },
+  },
+};
