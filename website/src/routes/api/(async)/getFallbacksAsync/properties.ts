ac6acfcc5c1386900944d86478c7c0c0671d1916
Elton Lobo <eltonsw00@gmail.com>
2024-07-26 00:11:45 +0530

    add `getFallbacksAsync` API reference

---
+++ website/src/routes/api/(async)/getFallbacksAsync/properties.ts
@@ -0,0 +1,67 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TSchema: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'BaseSchema',
+          href: '../BaseSchema/',
+          generics: [
+            'unknown',
+            'unknown',
+            {
+              type: 'custom',
+              name: 'BaseIssue',
+              href: '../BaseIssue/',
+              generics: ['unknown'],
+            },
+          ],
+        },
+        {
+          type: 'custom',
+          name: 'BaseSchemaAsync',
+          href: '../BaseSchemaAsync/',
+          generics: [
+            'unknown',
+            'unknown',
+            {
+              type: 'custom',
+              name: 'BaseIssue',
+              href: '../BaseIssue/',
+              generics: ['unknown'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  schema: {
+    type: {
+      type: 'custom',
+      name: 'TSchema',
+    },
+  },
+  values: {
+    type: {
+      type: 'custom',
+      name: 'Promise',
+      generics: [
+        {
+          type: 'custom',
+          name: 'InferFallbacks',
+          href: '../InferFallbacks/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'TSchema',
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
