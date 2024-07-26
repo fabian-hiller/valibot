c3ffc70ea2af7283599d8a29742bfd94a2a16725
Fabian Hiller <hillerfabian11@gmail.com>
2024-07-26 16:40:19 +0200

    Add description action with unit and type tests
    Co-authored-by: ⛵ <xcfox@users.noreply.github.com>

--- library/src/actions/index.ts
+++ library/src/actions/index.ts
@@ -8,6 +8,7 @@ export * from './checkItems/index.ts';
 export * from './creditCard/index.ts';
 export * from './cuid2/index.ts';
 export * from './decimal/index.ts';
+export * from './description/index.ts';
 export * from './email/index.ts';
 export * from './emoji/index.ts';
 export * from './empty/index.ts';
