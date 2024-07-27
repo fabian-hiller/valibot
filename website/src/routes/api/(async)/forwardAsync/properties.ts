0d6e1ab1823d726ead36e9504ddc9a30ecf22296
Elton Lobo <eltonsw00@gmail.com>
2024-07-27 14:30:27 +0530

    add `forwardAsync` API reference

---
+++ website/src/routes/api/(async)/forwardAsync/properties.ts
@@ -0,0 +1,110 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TInput: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'Record',
+          generics: ['string', 'unknown'],
+        },
+        {
+          type: 'custom',
+          name: 'ArrayLike',
+          generics: ['unknown'],
+        },
+      ],
+    },
+  },
+  TIssue: {
+    modifier: 'extends',
+    type: {
+      type: 'custom',
+      name: 'BaseIssue',
+      href: '../BaseIssue/',
+      generics: ['unknown'],
+    },
+  },
+  action: {
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'BaseValidation',
+          href: '../BaseValidation/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'TInput',
+            },
+            {
+              type: 'custom',
+              name: 'TInput',
+            },
+            {
+              type: 'custom',
+              name: 'TIssue',
+            },
+          ],
+        },
+        {
+          type: 'custom',
+          name: 'BaseValidationAsync',
+          href: '../BaseValidationAsync/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'TInput',
+            },
+            {
+              type: 'custom',
+              name: 'TInput',
+            },
+            {
+              type: 'custom',
+              name: 'TIssue',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  pathKeys: {
+    type: {
+      type: 'custom',
+      name: 'PathKeys',
+      href: '../PathKeys/',
+      generics: [
+        {
+          type: 'custom',
+          name: 'TInput',
+        },
+      ],
+    },
+  },
+  Action: {
+    type: {
+      type: 'custom',
+      name: 'BaseValidationAsync',
+      href: '../BaseValidationAsync/',
+      generics: [
+        {
+          type: 'custom',
+          name: 'TInput',
+        },
+        {
+          type: 'custom',
+          name: 'TInput',
+        },
+        {
+          type: 'custom',
+          name: 'TIssue',
+        },
+      ],
+    },
+  },
+};
