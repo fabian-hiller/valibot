6ec0279d352422b61c314c7ac4ceb0f5e7a4928b
Elton Lobo <eltonsw00@gmail.com>
2024-08-02 01:34:07 +0530

    add `objectWithRestAsync` API reference

---
+++ website/src/routes/api/(async)/objectWithRestAsync/properties.ts
@@ -0,0 +1,110 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TEntries: {
+    modifier: 'extends',
+    type: {
+      type: 'custom',
+      name: 'ObjectEntriesAsync',
+      href: '../ObjectEntriesAsync/',
+    },
+  },
+  TRest: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'BaseSchema',
+          href: '../BaseSchema/',
+          generics: [
+            'unknown',
+            'unknown',
+            {
+              type: 'custom',
+              name: 'BaseIssue',
+              href: '../BaseIssue/',
+              generics: ['unknown'],
+            },
+          ],
+        },
+        {
+          type: 'custom',
+          name: 'BaseSchemaAsync',
+          href: '../BaseSchemaAsync/',
+          generics: [
+            'unknown',
+            'unknown',
+            {
+              type: 'custom',
+              name: 'BaseIssue',
+              href: '../BaseIssue/',
+              generics: ['unknown'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  TMessage: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'ObjectWithRestIssue',
+              href: '../ObjectWithRestIssue/',
+            },
+          ],
+        },
+        'undefined',
+      ],
+    },
+  },
+  entries: {
+    type: {
+      type: 'custom',
+      name: 'TEntries',
+    },
+  },
+  rest: {
+    type: {
+      type: 'custom',
+      name: 'TRest',
+    },
+  },
+  message: {
+    type: {
+      type: 'custom',
+      name: 'TMessage',
+    },
+  },
+  Schema: {
+    type: {
+      type: 'custom',
+      name: 'ObjectWithRestSchemaAsync',
+      href: '../ObjectWithRestSchemaAsync/',
+      generics: [
+        {
+          type: 'custom',
+          name: 'TEntries',
+        },
+        {
+          type: 'custom',
+          name: 'TRest',
+        },
+        {
+          type: 'custom',
+          name: 'TMessage',
+        },
+      ],
+    },
+  },
+};
