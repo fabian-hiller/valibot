27b88b318f6e67cf54707131a4b4811b532f3cf1
Tatsunori Uchino <tats.u@live.jp>
2024-10-22 00:41:48 +0900

    Simplify

--- library/src/utils/_getCodePointCount/_getCodePointCountCount.ts
+++ library/src/utils/_getCodePointCount/_getCodePointCountCount.ts
@@ -8,16 +8,14 @@
  * @internal
  */
 export function _getCodePointCount(input: string): number {
-  let count: number = 0;
-  for (let i = 0; i < input.length; ) {
+  let count = 0;
+  for (let i = 0; i < input.length; count++) {
     // If codePoint were undefined here, we would have already got out of loop
-    const codePoint = input.codePointAt(i)!;
-    if (codePoint <= 65535) {
+    if (input.codePointAt(i)! <= 65535) {
       i++;
     } else {
       i += 2; // 2 characters (surrogate pair) in JS (UTF-16)
     }
-    count++;
   }
   return count;
 }
