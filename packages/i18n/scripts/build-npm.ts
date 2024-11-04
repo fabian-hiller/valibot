190c4d5b287ef1886f1d8cdb54a8f650da352eea
Fabian Hiller <hillerfabian11@gmail.com>
2024-11-03 21:58:02 -0500

    Add Farsi (fa) translations to various files

--- packages/i18n/scripts/build-npm.ts
+++ packages/i18n/scripts/build-npm.ts
@@ -5,6 +5,7 @@ import ar from '../src/ar';
 import ca from '../src/ca';
 import de from '../src/de';
 import es from '../src/es';
+import fa from '../src/fa';
 import fr from '../src/fr';
 import hu from '../src/hu';
 import id from '../src/id';
@@ -35,6 +36,7 @@ const languages = [
   ca,
   de,
   es,
+  fa,
   fr,
   hu,
   id,
