549cf701aab8f1b099dc15a2375cdef05cf5255d
Fabian Hiller <hillerfabian11@gmail.com>
2024-11-03 23:15:35 -0500

    Bump version of i18n to 1.0.0-beta.1

--- packages/i18n/scripts/build-jsr.ts
+++ packages/i18n/scripts/build-jsr.ts
@@ -103,7 +103,7 @@ for (const language of languages) {
   fs.writeFileSync(
     path.join(language.code, 'schema.ts'),
     `
-import { setSchemaMessage } from "jsr:@valibot/valibot@0";
+import { setSchemaMessage } from "jsr:@valibot/valibot@1";
 
 setSchemaMessage(
   ${language.schema.toString()},
@@ -125,7 +125,7 @@ setSchemaMessage(
     fs.writeFileSync(
       path.join(language.code, `${reference}.ts`),
       `
-import { setSpecificMessage, ${reference} } from "jsr:@valibot/valibot@0";
+import { setSpecificMessage, ${reference} } from "jsr:@valibot/valibot@1";
 
 setSpecificMessage(
   ${reference},
