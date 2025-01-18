7a6072bfbf6ca1937ba6866c2b0a8c7ec04677dc
SerKo <serko.dev@gmail.com>
2025-01-19 03:12:47 +0800

    feat: export `entriesFromObjects` in utils

--- library/src/utils/index.ts
+++ library/src/utils/index.ts
@@ -8,6 +8,7 @@ export * from './_isValidObjectKey/index.ts';
 export * from './_joinExpects/index.ts';
 export * from './_stringify/index.ts';
 export * from './entriesFromList/index.ts';
+export * from './entriesFromObjects/index.ts';
 export * from './getDotPath/index.ts';
 export * from './isOfKind/index.ts';
 export * from './isOfType/index.ts';
