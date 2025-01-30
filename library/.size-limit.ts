dda7839957b8df80abf8cdc74ceb250af64a956b
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-01-30 12:53:02 +0100

    chore: propose an example guard with size-limit

--- library/.size-limit.ts
+++ library/.size-limit.ts
@@ -5,6 +5,27 @@ module.exports = [
     name: `import * as v from 'valibot' (ESM)`,
     path: ['dist/index.js'],
     import: '*',
-    limit: '10.4KB',
+    limit: '10.35KB',
+  },
+  // Core
+  {
+    name: `import { parser } from 'valibot' (ESM)`,
+    path: ['dist/index.js'],
+    import: '{ parser }',
+    limit: '335KB',
+  },
+  // Functions
+  {
+    name: `import { pipe } from 'valibot' (ESM)`,
+    path: ['dist/index.js'],
+    import: '{ pipe }',
+    limit: '370B',
+  },
+  // Schemas
+  {
+    name: `import { array } from 'valibot' (ESM)`,
+    path: ['dist/index.js'],
+    import: '{ array }',
+    limit: '815B',
   },
 ] satisfies SizeLimitConfig;
