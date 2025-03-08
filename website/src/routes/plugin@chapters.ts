ae7590a45507d4c08c3ce3f329c2d1691b792324
Fabian Hiller <hillerfabian11@gmail.com>
2025-03-07 23:34:09 -0500

    Refactor and fix cookie handling for chapters and theme plugins

--- website/src/routes/plugin@chapters.ts
+++ website/src/routes/plugin@chapters.ts
@@ -1,19 +1,30 @@
-import { routeAction$, routeLoader$ } from '@builder.io/qwik-city';
+import {
+  type RequestEventAction,
+  routeAction$,
+  routeLoader$,
+} from '@builder.io/qwik-city';
 
 const COOKIE_NAME = 'chapters';
+
+/**
+ * Returns the value of the chapters cookie.
+ */
+function getCookie(request: RequestEventAction) {
+  return request.cookie.get(COOKIE_NAME)?.value ?? 'true';
+}
 
 /**
  * Returns whether chapters are enabled.
  */
 export const useChapters = routeLoader$(
-  ({ cookie }) => (cookie.get(COOKIE_NAME)?.value ?? 'true') === 'true'
+  (request) => getCookie(request) === 'true'
 );
 
 /**
  * Toggles the chapters by changing the chapters cookie.
  */
-export const useChaptersToggle = routeAction$((_, { cookie }) => {
-  cookie.set(COOKIE_NAME, `${!(cookie.get(COOKIE_NAME)?.value === 'true')}`, {
+export const useChaptersToggle = routeAction$((_, request) => {
+  request.cookie.set(COOKIE_NAME, `${getCookie(request) !== 'true'}`, {
     httpOnly: true,
     maxAge: 31557600, // 1 year
     path: '/',
