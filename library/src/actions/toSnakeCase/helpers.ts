86bc133a0b1664abefb92b4aa730006979c99b69
Elton Lobo <eltonsw00@gmail.com>
2025-03-16 00:33:51 +0530

    use whitespace list to find whitespaces in the input
    While transforming a string to a case such as snake case, the runtime
    and type system have to find whitespace characters. To keep the
    runtime & related types in sync, create and use a whitespace list.

--- library/src/actions/toSnakeCase/helpers.ts
+++ library/src/actions/toSnakeCase/helpers.ts
@@ -1,11 +1,47 @@
+/*
+  Contains all of the characters that are removed by the `trim` method
+
+  This is done so that the runtime and related types are in sync, as there is no trim 
+  equivalent in the type system
+*/
+export const whitespaces = [
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#white_space
+  '\t',
+  '\v',
+  '\f',
+  ' ',
+  '\u00A0',
+  '\uFEFF',
+  '\u1680',
+  '\u2000',
+  '\u2001',
+  '\u2002',
+  '\u2003',
+  '\u2004',
+  '\u2005',
+  '\u2006',
+  '\u2007',
+  '\u2008',
+  '\u2009',
+  '\u200A',
+  '\u202F',
+  '\u205F',
+  '\u3000',
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#line_terminators
+  '\n',
+  '\r',
+  '\u2028',
+  '\u2029',
+] as const;
+
 const separators: Set<string> = new Set<string>([
-  '', // represents all whitespaces (trim the character before the membership check)
+  ...whitespaces,
   '_',
   '-',
   '.',
 ]);
 
-const isSeparator = (ch: string): boolean => separators.has(ch.trim());
+const isSeparator = (ch: string): boolean => separators.has(ch);
 
 const isUpperCase = (ch: string): boolean =>
   ch === ch.toUpperCase() && ch !== ch.toLowerCase();
--- library/src/actions/toSnakeCaseKeys/types.ts
+++ library/src/actions/toSnakeCaseKeys/types.ts
@@ -1,3 +1,4 @@
+import type { whitespaces } from '../toSnakeCase/helpers.ts';
 import type {
   ObjectInput,
   SelectedStringKeys,
@@ -18,34 +19,7 @@ export type Output<
   }
 >;
 
-type Whitespace =
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#white_space
-  | '\t'
-  | '\v'
-  | '\f'
-  | ' '
-  | '\u00A0'
-  | '\uFEFF'
-  | '\u1680'
-  | '\u2000'
-  | '\u2001'
-  | '\u2002'
-  | '\u2003'
-  | '\u2004'
-  | '\u2005'
-  | '\u2006'
-  | '\u2007'
-  | '\u2008'
-  | '\u2009'
-  | '\u200A'
-  | '\u202F'
-  | '\u205F'
-  | '\u3000'
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#line_terminators
-  | '\n'
-  | '\r'
-  | '\u2028'
-  | '\u2029';
+type Whitespace = (typeof whitespaces)[number];
 
 type SnakeCaseSeparator = '_';
 
