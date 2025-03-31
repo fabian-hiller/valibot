78d5e46fa985cbd4576c02e96109929d652dcdb6
Elton Lobo <eltonsw00@gmail.com>
2025-03-31 20:21:11 +0530

    test commit

--- codemod/zod-to-valibot/src/transform/index.ts
+++ codemod/zod-to-valibot/src/transform/index.ts
@@ -2,13 +2,16 @@ import type { Transform } from 'jscodeshift';
 
 const transform: Transform = (fileInfo, api) => {
   const j = api.jscodeshift;
+
   const root = j(fileInfo.source);
+
   root
     .find(j.VariableDeclaration)
     .filter((path) => path.node.kind === 'var')
     .forEach((path) => {
       path.node.kind = 'let';
     });
+
   return root.toSource();
 };
 
