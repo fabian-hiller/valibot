1d5df4e08bdd84949e4e3e0a88d9ee6bbced1a30
Ben Durrant <ben.durrant@marketdojo.com>
2025-04-08 11:42:21 +0100

    add pages to site

---
+++ website/src/routes/api/(types)/JsonAction/properties.ts
@@ -0,0 +1,117 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TInput: {
+    modifier: 'extends',
+    type: 'string',
+  },
+  TReviver: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'function',
+          params: [
+            {
+              name: 'this',
+              type: 'any',
+            },
+            {
+              name: 'key',
+              type: 'string',
+            },
+            {
+              name: 'value',
+              type: 'any',
+            },
+          ],
+          return: 'any',
+        },
+        'undefined',
+      ],
+    },
+  },
+  TMessage: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'JsonIssue',
+              href: '../JsonIssue/',
+              generics: [
+                {
+                  type: 'custom',
+                  name: 'TInput',
+                },
+              ],
+            },
+          ],
+        },
+        'undefined',
+      ],
+    },
+  },
+  BaseTransformation: {
+    modifier: 'extends',
+    type: {
+      type: 'custom',
+      name: 'BaseTransformation',
+      href: '../BaseTransformation/',
+      generics: [
+        {
+          type: 'custom',
+          name: 'TInput',
+        },
+        {
+          type: 'custom',
+          name: 'unknown',
+        },
+        {
+          type: 'custom',
+          name: 'JsonIssue',
+          href: '../JsonIssue/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'TInput',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  type: {
+    type: {
+      type: 'string',
+      value: 'json',
+    },
+  },
+  reference: {
+    type: {
+      type: 'custom',
+      modifier: 'typeof',
+      name: 'json',
+      href: '../json/',
+    },
+  },
+  reviver: {
+    type: {
+      type: 'custom',
+      name: 'TReviver',
+    },
+  },
+  message: {
+    type: {
+      type: 'custom',
+      name: 'TMessage',
+    },
+  },
+};
