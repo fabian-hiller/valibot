1ac9e9f210555908846b02430a6cfae265ee1f5e
Ben Durrant <ben.durrant@marketdojo.com>
2025-04-08 11:42:21 +0100

    add pages to site

---
+++ website/src/routes/api/(actions)/json/properties.ts
@@ -0,0 +1,95 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TInput: {
+    modifier: 'extends',
+    type: 'string',
+  },
+  TReviver: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'function',
+          params: [
+            {
+              name: 'this',
+              type: 'any',
+            },
+            {
+              name: 'key',
+              type: 'string',
+            },
+            {
+              name: 'value',
+              type: 'any',
+            },
+          ],
+          return: 'any',
+        },
+        'undefined',
+      ],
+    },
+  },
+  TMessage: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'ErrorMessage',
+          href: '../ErrorMessage/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'JsonIssue',
+              href: '../JsonIssue/',
+              generics: [
+                {
+                  type: 'custom',
+                  name: 'TInput',
+                },
+              ],
+            },
+          ],
+        },
+        'undefined',
+      ],
+    },
+  },
+  reviver: {
+    type: {
+      type: 'custom',
+      name: 'TReviver',
+    },
+  },
+  message: {
+    type: {
+      type: 'custom',
+      name: 'TMessage',
+    },
+  },
+  Action: {
+    type: {
+      type: 'custom',
+      name: 'JsonAction',
+      href: '../JsonAction/',
+      generics: [
+        {
+          type: 'custom',
+          name: 'TInput',
+        },
+        {
+          type: 'custom',
+          name: 'TReviver',
+        },
+        {
+          type: 'custom',
+          name: 'TMessage',
+        },
+      ],
+    },
+  },
+};
