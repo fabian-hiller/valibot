081cf885ef8b527f64f28424dca253835fe58aae
Ben Durrant <ben.durrant@marketdojo.com>
2025-04-11 18:23:44 +0100

    stringify docs

---
+++ website/src/routes/api/(types)/JsonStringifyIssue/properties.ts
@@ -0,0 +1,53 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TInput: {
+    modifier: 'extends',
+    type: 'any',
+  },
+  BaseIssue: {
+    modifier: 'extends',
+    type: {
+      type: 'custom',
+      name: 'BaseIssue',
+      href: '../BaseIssue/',
+      generics: [
+        {
+          type: 'custom',
+          name: 'TInput',
+        },
+      ],
+    },
+  },
+  kind: {
+    type: {
+      type: 'string',
+      value: 'transformation',
+    },
+  },
+  type: {
+    type: {
+      type: 'string',
+      value: 'json_stringify',
+    },
+  },
+  expected: {
+    type: 'null',
+  },
+  received: {
+    type: {
+      type: 'template',
+      parts: [
+        {
+          type: 'string',
+          value: '"',
+        },
+        'string',
+        {
+          type: 'string',
+          value: '"',
+        },
+      ],
+    },
+  },
+};
