098d671786b5c47086ca79cfc538da44c90d77a8
Elton Lobo <eltonsw00@gmail.com>
2025-04-18 00:21:45 +0530

    improve zod to valibot intersection tests

--- codemod/zod-to-valibot/__testfixtures__/intersection-schema/input.ts
+++ codemod/zod-to-valibot/__testfixtures__/intersection-schema/input.ts
@@ -3,8 +3,15 @@ import { z } from "zod";
 const Person = z.object({
   name: z.string(),
 });
+
 const Employee = z.object({
   role: z.string(),
 });
+
 const Schema1 = z.intersection(Person, Employee);
-const Schema2 = Person.and(Employee);
+
+const Student = z.object({
+  favSubject: z.string(),
+});
+
+const Schema2 = Person.and(Employee).and(Student);
--- codemod/zod-to-valibot/__testfixtures__/intersection-schema/output.ts
+++ codemod/zod-to-valibot/__testfixtures__/intersection-schema/output.ts
@@ -1,10 +1,17 @@
 import * as v from "valibot";
 
 const Person = v.object({
-	name: v.string(),
+  name: v.string(),
 });
+
 const Employee = v.object({
-	role: v.string(),
+  role: v.string(),
 });
+
 const Schema1 = v.intersect([Person, Employee]);
-const Schema2 = v.intersect([Person, Employee]);
+
+const Student = v.object({
+  favSubject: v.string(),
+});
+
+const Schema2 = v.intersect([Person, Employee, Student]);
