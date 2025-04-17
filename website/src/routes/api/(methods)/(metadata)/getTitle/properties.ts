121cffb82ac8775d95913b9f6b6b954c9b2eadcb
Ben Durrant <ben.durrant@marketdojo.com>
2025-04-17 23:55:09 +0100

    add docs

---
+++ website/src/routes/api/(methods)/(metadata)/getTitle/properties.ts
@@ -0,0 +1,164 @@
+import type { PropertyProps } from '~/components';
+
+export const properties: Record<string, PropertyProps> = {
+  TSchema: {
+    modifier: 'extends',
+    type: {
+      type: 'union',
+      options: [
+        {
+          type: 'custom',
+          name: 'SchemaWithPipe',
+          href: '../SchemaWithPipe/',
+          generics: [
+            {
+              type: 'tuple',
+              modifier: 'readonly',
+              items: [
+                {
+                  type: 'custom',
+                  name: 'BaseSchema',
+                  href: '../BaseSchema/',
+                  generics: [
+                    'unknown',
+                    'unknown',
+                    {
+                      type: 'custom',
+                      name: 'BaseIssue',
+                      href: '../BaseIssue/',
+                      generics: ['unknown'],
+                    },
+                  ],
+                },
+                {
+                  type: 'array',
+                  spread: true,
+                  item: {
+                    type: 'custom',
+                    name: 'PipeItem',
+                    href: '../PipeItem/',
+                    generics: [
+                      'any',
+                      'unknown',
+                      {
+                        type: 'custom',
+                        name: 'BaseIssue',
+                        href: '../BaseIssue/',
+                        generics: ['unknown'],
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'custom',
+          name: 'SchemaWithPipeAsync',
+          href: '../SchemaWithPipeAsync/',
+          generics: [
+            {
+              type: 'tuple',
+              modifier: 'readonly',
+              items: [
+                {
+                  type: 'union',
+                  options: [
+                    {
+                      type: 'custom',
+                      name: 'BaseSchema',
+                      href: '../BaseSchema/',
+                      generics: [
+                        'unknown',
+                        'unknown',
+                        {
+                          type: 'custom',
+                          name: 'BaseIssue',
+                          href: '../BaseIssue/',
+                          generics: ['unknown'],
+                        },
+                      ],
+                    },
+                    {
+                      type: 'custom',
+                      name: 'BaseSchemaAsync',
+                      href: '../BaseSchemaAsync/',
+                      generics: [
+                        'unknown',
+                        'unknown',
+                        {
+                          type: 'custom',
+                          name: 'BaseIssue',
+                          href: '../BaseIssue/',
+                          generics: ['unknown'],
+                        },
+                      ],
+                    },
+                  ],
+                },
+                {
+                  type: 'array',
+                  spread: true,
+                  item: {
+                    type: 'union',
+                    options: [
+                      {
+                        type: 'custom',
+                        name: 'PipeItem',
+                        href: '../PipeItem/',
+                        generics: [
+                          'any',
+                          'unknown',
+                          {
+                            type: 'custom',
+                            name: 'BaseIssue',
+                            href: '../BaseIssue/',
+                            generics: ['unknown'],
+                          },
+                        ],
+                      },
+                      {
+                        type: 'custom',
+                        name: 'PipeItemAsync',
+                        href: '../PipeItemAsync/',
+                        generics: [
+                          'any',
+                          'unknown',
+                          {
+                            type: 'custom',
+                            name: 'BaseIssue',
+                            href: '../BaseIssue/',
+                            generics: ['unknown'],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  schema: {
+    type: {
+      type: 'custom',
+      name: 'TSchema',
+    },
+  },
+  title: {
+    type: {
+      type: 'custom',
+      name: 'ExtractTitle',
+      generics: [
+        {
+          type: 'custom',
+          name: 'TSchema',
+        },
+      ],
+    },
+  },
+};
