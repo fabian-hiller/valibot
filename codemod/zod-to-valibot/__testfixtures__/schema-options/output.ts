2998535018a8075d50d9861889aac62d97ae186e
Elton Lobo <eltonsw00@gmail.com>
2025-04-21 17:35:37 +0530

    improve zod to valibot schema options tests

--- codemod/zod-to-valibot/__testfixtures__/schema-options/output.ts
+++ codemod/zod-to-valibot/__testfixtures__/schema-options/output.ts
@@ -12,9 +12,9 @@ const Schema5 = v.string();
 const Schema6 = v.literal("bar");
 // description
 const Schema7 = v.pipe(v.string(), v.description("some description"));
-const schema7Description = Schema7.pipe[1].description;
+const schema7Description = Schema7.pipe.find(action => action.type === "description")?.description;
 const Schema8 = v.pipe(v.literal("hello"), v.description('some description (literal "hello")'));
-const schema8Description = Schema8.pipe[1].description;
+const schema8Description = Schema8.pipe.find(action => action.type === "description")?.description;
 // coerce
 const Schema9 = v.pipe(v.unknown(), v.transform(String));
 // errorMap - supported by Valibot but incompatible
@@ -25,4 +25,4 @@ const Schema11 = v.literal("world");
 const Schema12 = v.string("must be a string (msg)");
 const Schema13 = v.literal("bot", 'must be "bot" (msg)');
 const Schema14 = v.pipe(v.unknown(), v.transform(String), v.description("some description"));
-const schema14Description = Schema14.pipe[2].description;
+const schema14Description = Schema14.pipe.find(action => action.type === "description")?.description;;
