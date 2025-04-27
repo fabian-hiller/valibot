410ee5f57ff6695f9b74058bac5136792958981c
Elton Lobo <eltonsw00@gmail.com>
2025-04-27 14:01:04 +0530

    improve zod to valibot coerce number schema tests

--- codemod/zod-to-valibot/__testfixtures__/coerce-number-schema/output.ts
+++ codemod/zod-to-valibot/__testfixtures__/coerce-number-schema/output.ts
@@ -1,6 +1,6 @@
 import * as v from "valibot";
 
-const Schema1 = v.pipe(v.unknown(), v.number());
-const Schema2 = v.pipe(v.unknown(), v.number());
-const Schema3 = v.pipe(v.unknown(), v.number(), v.finite());
-const Schema4 = v.pipe(v.unknown(), v.number(), v.multipleOf(12));
+const Schema1 = v.pipe(v.unknown(), v.transform(Number), v.number());
+const Schema2 = v.pipe(v.unknown(), v.transform(Number), v.number());
+const Schema3 = v.pipe(v.unknown(), v.transform(Number), v.number(), v.finite());
+const Schema4 = v.pipe(v.unknown(), v.transform(Number), v.number(), v.multipleOf(12));
