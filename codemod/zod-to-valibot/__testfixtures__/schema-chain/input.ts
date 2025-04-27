5a909f01bb8b711971aa24ba655fa6330436a279
Elton Lobo <eltonsw00@gmail.com>
2025-04-27 13:03:08 +0530

    improve zod to valibot schema chain tests

--- codemod/zod-to-valibot/__testfixtures__/schema-chain/input.ts
+++ codemod/zod-to-valibot/__testfixtures__/schema-chain/input.ts
@@ -1,3 +1,5 @@
 import { z } from "zod";
 
-const EmailSchema = z.string().trim().email();
+const Schema1 = z.string().trim().email();
+const output1 = z.string().parse("valibot@example.com");
+const output2 = z.string().trim().email().parse("valibot@example.com");
--- codemod/zod-to-valibot/__testfixtures__/schema-chain/output.ts
+++ codemod/zod-to-valibot/__testfixtures__/schema-chain/output.ts
@@ -1,3 +1,5 @@
 import * as v from "valibot";
 
-const EmailSchema = v.pipe(v.string(), v.trim(), v.email());
+const Schema1 = v.pipe(v.string(), v.trim(), v.email());
+const output1 = v.parse(v.string(), "valibot@example.com");
+const output2 = v.parse(v.pipe(v.string(), v.trim(), v.email()), "valibot@example.com");
