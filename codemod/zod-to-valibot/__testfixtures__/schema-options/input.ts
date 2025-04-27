36475986d24ac4bf81caa2aabd75a87ee91b5afc
Elton Lobo <eltonsw00@gmail.com>
2025-04-27 13:50:02 +0530

    improve zod to valibot schema options tests

--- codemod/zod-to-valibot/__testfixtures__/schema-options/input.ts
+++ codemod/zod-to-valibot/__testfixtures__/schema-options/input.ts
@@ -7,7 +7,7 @@ const Schema2 = z.literal("valibot", {invalid_type_error: 'must be "valibot"'});
 // message
 const Schema3 = z.string({message: "must be a string"});
 const Schema4 = z.literal("foo", {message: 'must be "foo"'});
-// required_error - not supported by Valibot
+// required_error
 const Schema5 = z.string({required_error: "is required"});
 const Schema6 = z.literal("bar", {required_error: '"bar" is required'});
 // description
@@ -22,7 +22,18 @@ const Schema10 = z.string({errorMap: () => ({message: "some message"})});
 const Schema11 = z.literal("world", {errorMap: () => ({message: 'some message (literal "world")'})});
 
 // ------------ multiple ------------
-const Schema12 = z.string({invalid_type_error: "must be a string (invalid type)", message: "must be a string (msg)"});
-const Schema13 = z.literal("bot", {invalid_type_error: 'must be "bot" (invalid type)', message: 'must be "bot" (msg)'});
-const Schema14 = z.string({coerce: true, description: "some description"});
-const schema14Description = Schema14.description;
+// invalid_type_error + required_error
+const Schema12 = z.string({invalid_type_error: "must be a string", required_error: "is required"});
+const Schema13 = z.literal("bot", {invalid_type_error: 'must be "bot"', required_error: '"bot" is required'});
+// invalid_type_error + message
+const Schema14 = z.string({invalid_type_error: "must be a string", message: "must be a string (msg)"});
+const Schema15 = z.literal("bot", {invalid_type_error: 'must be "bot"', message: 'must be "bot" (msg)'});
+// required_error + message
+const Schema16 = z.string({required_error: "is required", message: "must be a string (msg)"});
+const Schema17 = z.literal("bot", {required_error: '"bot" is required', message: 'must be "bot" (msg)'});
+// invalid_type_error + required_error + message
+const Schema18 = z.string({invalid_type_error: "must be a string", required_error: "is required", message: "must be a string (msg)"});
+const Schema19 = z.literal("bot", {invalid_type_error: 'must be "bot"', required_error: '"bot" is required', message: 'must be "bot" (msg)'});
+// coerce + description
+const Schema20 = z.string({coerce: true, description: "some description"});
+const schema20Description = Schema20.description;
--- codemod/zod-to-valibot/__testfixtures__/schema-options/output.ts
+++ codemod/zod-to-valibot/__testfixtures__/schema-options/output.ts
@@ -7,9 +7,9 @@ const Schema2 = v.literal("valibot", 'must be "valibot"');
 // message
 const Schema3 = v.string("must be a string");
 const Schema4 = v.literal("foo", 'must be "foo"');
-// required_error - not supported by Valibot
-const Schema5 = v.string();
-const Schema6 = v.literal("bar");
+// required_error
+const Schema5 = v.string(issue => issue.input === undefined ? "is required" : issue.message);
+const Schema6 = v.literal("bar", issue => issue.input === undefined ? '"bar" is required' : issue.message);
 // description
 const Schema7 = v.pipe(v.string(), v.description("some description"));
 const schema7Description = Schema7.pipe.find(action => action.type === "description")?.description;
@@ -22,7 +22,18 @@ const Schema10 = v.string();
 const Schema11 = v.literal("world");
 
 // ------------ multiple ------------
-const Schema12 = v.string("must be a string (msg)");
-const Schema13 = v.literal("bot", 'must be "bot" (msg)');
-const Schema14 = v.pipe(v.unknown(), v.transform(String), v.description("some description"));
-const schema14Description = Schema14.pipe.find(action => action.type === "description")?.description;;
+// invalid_type_error + required_error
+const Schema12 = v.string(issue => issue.input === undefined ? "is required" : "must be a string");
+const Schema13 = v.literal("bot", issue => issue.input === undefined ? 'must be "bot"' : '"bot" is required');
+// invalid_type_error + message
+const Schema14 = v.string("must be a string (msg)");
+const Schema15 = v.literal("bot", 'must be "bot" (msg)');
+// required_error + message
+const Schema16 = v.string("must be a string (msg)");
+const Schema17 = v.literal("bot", 'must be "bot" (msg)');
+// invalid_type_error + required_error + message
+const Schema18 = v.string("must be a string (msg)");
+const Schema19 = v.literal("bot", 'must be "bot" (msg)');
+// coerce + description
+const Schema20 = v.pipe(v.unknown(), v.transform(String), v.description("some description"));
+const schema20Description = Schema20.pipe.find(action => action.type === "description")?.description;
