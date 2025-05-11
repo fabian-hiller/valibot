0e13f24d694452bdba106886fbe9d5a85189c7e5
Elton Lobo <eltonsw00@gmail.com>
2025-05-11 15:14:34 +0530

    add zod to valibot validation msg transform

--- codemod/zod-to-valibot/src/transform/schemas-and-properties/constants.ts
+++ codemod/zod-to-valibot/src/transform/schemas-and-properties/constants.ts
@@ -16,24 +16,49 @@ export const ZOD_SCHEMAS = [
 export const ZOD_VALIDATORS = [
   'describe',
   'email',
+  'endsWith',
   'finite',
+  'includes',
+  'length',
   'max',
   'min',
   'multipleOf',
+  'regex',
+  'size',
+  'startsWith',
   'trim',
   'url',
   'gt',
+  'gte',
   'lt',
+  'lte',
 ] as const;
 
-export const VALIDATOR_TO_ACTION: Partial<
-  Record<(typeof ZOD_VALIDATORS)[number], string>
-> = {
+type ZodValidator = (typeof ZOD_VALIDATORS)[number];
+
+export const VALIDATOR_TO_ACTION: Partial<Record<ZodValidator, string>> = {
   describe: 'description',
   max: 'maxValue',
   min: 'minValue',
   gt: 'gtValue',
+  gte: 'minValue',
   lt: 'ltValue',
+  lte: 'maxValue',
+};
+
+export const VALIDATOR_TO_NUM_ARGS: Partial<Record<ZodValidator, number>> = {
+  endsWith: 2,
+  gte: 2,
+  includes: 2,
+  length: 2,
+  lt: 2,
+  lte: 2,
+  max: 2,
+  min: 2,
+  multipleOf: 2,
+  regex: 2,
+  size: 2,
+  startsWith: 2,
 };
 
 export const ZOD_PROPERTIES = ['description'] as const;
