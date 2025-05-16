0fb93d1d94c984f7bd5ca8165c9ea08ed06e9f79
Elton Lobo <eltonsw00@gmail.com>
2025-05-16 23:43:36 +0530

    add zod to valibot literal schema transform

--- codemod/zod-to-valibot/src/transform/schemas-and-links/schemas/literal/literal.ts
+++ codemod/zod-to-valibot/src/transform/schemas-and-links/schemas/literal/literal.ts
@@ -7,9 +7,20 @@ export function transformLiteral(
   schemaOptions: SchemaOptionsToASTVal,
   argsExceptOptions: j.CallExpression['arguments']
 ) {
+  let schemaName: 'null' | 'undefined' | 'literal' = 'literal';
+  // The `literal` schema has at least one argument
+  // todo: find a way to check the assumption is correct and bail out if required
+  const [valArg] = argsExceptOptions;
+  if (
+    valArg.type === 'NullLiteral' ||
+    (valArg.type === 'Identifier' && valArg.name === 'undefined')
+  ) {
+    argsExceptOptions = argsExceptOptions.slice(1);
+    schemaName = valArg.type === 'NullLiteral' ? 'null' : 'undefined';
+  }
   return getSchemaWithOptionalDescription(
     valibotIdentifier,
-    getSchema(valibotIdentifier, 'literal', schemaOptions, argsExceptOptions),
+    getSchema(valibotIdentifier, schemaName, schemaOptions, argsExceptOptions),
     schemaOptions.description
   );
 }
