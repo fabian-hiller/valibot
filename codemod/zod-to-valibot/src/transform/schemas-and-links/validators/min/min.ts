32fd57cf6a9f0b54b1b5d9ecae1ac820f5a6bfa3
Elton Lobo <eltonsw00@gmail.com>
2025-05-17 17:08:04 +0530

    remove validator maps from the codemod

---
+++ codemod/zod-to-valibot/src/transform/schemas-and-links/validators/min/min.ts
@@ -0,0 +1,19 @@
+import j from 'jscodeshift';
+import { splitLastArg } from '../../helpers';
+import { getValidatorMsg } from '../helpers';
+
+export function transformMin(
+  valibotIdentifier: string,
+  args: j.CallExpression['arguments']
+) {
+  const { firstArgs: argsExceptOptions, lastArg } = splitLastArg(2, args);
+  const msgArg = getValidatorMsg(lastArg);
+
+  return j.callExpression(
+    j.memberExpression(
+      j.identifier(valibotIdentifier),
+      j.identifier('minValue')
+    ),
+    [...argsExceptOptions, ...(msgArg !== null ? [msgArg] : [])]
+  );
+}
