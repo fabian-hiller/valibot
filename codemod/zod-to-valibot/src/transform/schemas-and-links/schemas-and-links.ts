bb642abc176cb14d5cb49d1d73b8672888c189a2
Elton Lobo <eltonsw00@gmail.com>
2025-05-18 10:46:00 +0530

    use `splitLastArg` while transforming zod schemas

--- codemod/zod-to-valibot/src/transform/schemas-and-links/schemas-and-links.ts
+++ codemod/zod-to-valibot/src/transform/schemas-and-links/schemas-and-links.ts
@@ -13,6 +13,7 @@ import {
   ZOD_UNCOERCEABLE_SCHEMAS,
   ZOD_VALIDATORS,
 } from './constants';
+import { splitLastArg } from './helpers';
 import { transformDescription } from './properties';
 import {
   transformBigint,
@@ -131,22 +132,6 @@ function toValibotSchemaExp(
       assertNever(zodSchemaName);
     }
   }
-}
-
-function splitLastArg(
-  maxArgs: number,
-  args: j.CallExpression['arguments']
-): {
-  firstArgs: j.CallExpression['arguments'];
-  lastArg: j.CallExpression['arguments'][number] | null;
-} {
-  let firstArgs = args;
-  let lastArg: j.CallExpression['arguments'][number] | null = null;
-  if (args.length === maxArgs && maxArgs > 0) {
-    firstArgs = args.slice(0, args.length - 1);
-    lastArg = args[args.length - 1];
-  }
-  return { firstArgs, lastArg };
 }
 
 function toValibotActionExp(
