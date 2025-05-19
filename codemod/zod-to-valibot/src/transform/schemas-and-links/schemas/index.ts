8c71cd6258cf3aeaf03cf4a81930be85236c225b
Elton Lobo <eltonsw00@gmail.com>
2025-05-19 14:53:24 +0530

    add zod to valibot `optional` schema transform

--- codemod/zod-to-valibot/src/transform/schemas-and-links/schemas/index.ts
+++ codemod/zod-to-valibot/src/transform/schemas-and-links/schemas/index.ts
@@ -2,6 +2,7 @@ export * from './bigint/bigint';
 export * from './boolean';
 export * from './date';
 export * from './number';
+export * from './optional';
 export * from './string';
 export * from './literal';
 export type { SchemaOptionsToASTVal } from './types';
