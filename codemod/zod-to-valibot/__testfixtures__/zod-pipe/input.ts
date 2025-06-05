67a111598a853397041ca3024ef8c3f191f78a19
Elton Lobo <eltonsw00@gmail.com>
2025-06-05 21:58:44 +0530

    remove zod to valibot pipe tests

--- codemod/zod-to-valibot/__testfixtures__/zod-pipe/input.ts
+++
@@ -1,3 +0,0 @@
-import { z } from "zod";
-
-const Schema = z.string().transform((val) => val.length).pipe(z.number().min(12));
--- codemod/zod-to-valibot/__testfixtures__/zod-pipe/output.ts
+++
@@ -1,3 +0,0 @@
-import * as v from "valibot";
-
-const Schema = v.pipe(v.string(), v.transform((val) => val.length), v.number(), v.minValue(12));
