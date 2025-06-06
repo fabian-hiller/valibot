693b993d37e2aefcd206d82b9ca4cbedfb4e8218
Elton Lobo <eltonsw00@gmail.com>
2025-06-06 21:08:26 +0530

    improve zod `passthrough` transform

--- codemod/zod-to-valibot/__testfixtures__/object-passthrough/output.ts
+++ codemod/zod-to-valibot/__testfixtures__/object-passthrough/output.ts
@@ -1,5 +1,11 @@
 import * as v from "valibot";
 
-const Schema1 = v.looseObject({name: v.string(), age: v.number()});
-const Schema2 = v.object({name: v.string(), age: v.number()});
-const Schema3 = v.looseObject(Schema2.entries); 
+const Schema1 = v.looseObject({key: v.string()});
+const Schema2 = v.looseObject({key: v.string()}, "some message");
+const Schema3 = v.pipe(
+  v.object({key: v.string()}),
+  v.description("some description"),
+  v.passthrough()
+);
+const Schema4 = v.object({key: v.string()});
+const Schema5 = v.pipe(Schema4, v.passthrough());
--- codemod/zod-to-valibot/src/transform/schemas-and-links/methods/passthrough/passthrough.ts
+++ codemod/zod-to-valibot/src/transform/schemas-and-links/methods/passthrough/passthrough.ts
@@ -1,17 +1,35 @@
 import j from 'jscodeshift';
+import { addToPipe } from '../../helpers';
 
 export function transformPassthrough(
   valibotIdentifier: string,
   schemaExp: j.CallExpression | j.MemberExpression | j.Identifier
-  // args: j.CallExpression['arguments']
 ) {
-  return j.callExpression(
-    j.memberExpression(
-      j.identifier(valibotIdentifier),
-      j.identifier('looseObject')
-    ),
-    j.CallExpression.check(schemaExp)
-      ? schemaExp.arguments
-      : [j.memberExpression(schemaExp, j.identifier('entries'))]
+  if (
+    schemaExp.type === 'CallExpression' &&
+    schemaExp.callee.type === 'MemberExpression' &&
+    schemaExp.callee.object.type === 'Identifier' &&
+    schemaExp.callee.object.name === valibotIdentifier &&
+    schemaExp.callee.property.type === 'Identifier' &&
+    schemaExp.callee.property.name === 'object'
+  ) {
+    return j.callExpression(
+      j.memberExpression(
+        j.identifier(valibotIdentifier),
+        j.identifier('looseObject')
+      ),
+      schemaExp.arguments
+    );
+  }
+  return addToPipe(
+    valibotIdentifier,
+    schemaExp,
+    j.callExpression(
+      j.memberExpression(
+        j.identifier(valibotIdentifier),
+        j.identifier('passthrough')
+      ),
+      []
+    )
   );
 }
