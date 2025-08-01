2e8f9bffa9ce0b4683fc65d8af7330defbc97ef0
paoloricciuti <ricciutipaolo@gmail.com>
2025-08-01 19:11:38 +0200

    fix: properly migrate `extend` and `merge`

--- codemod/zod-to-valibot/src/transform/schemas-and-links/methods/extend/extend.ts
+++ codemod/zod-to-valibot/src/transform/schemas-and-links/methods/extend/extend.ts
@@ -1,12 +1,71 @@
 import j from 'jscodeshift';
 
+function isInlineObjectCall(exp: j.CallExpression): boolean {
+  return (
+    exp.callee.type === 'MemberExpression' &&
+    exp.callee.property.type === 'Identifier' &&
+    exp.callee.property.name === 'object'
+  );
+}
+
+function extractObjectProperties(
+  exp: j.CallExpression
+): j.ObjectExpression['properties'] {
+  const firstArg = exp.arguments[0];
+  if (firstArg && firstArg.type === 'ObjectExpression') {
+    return firstArg.properties;
+  }
+  return [];
+}
+
 export function transformExtend(
   valibotIdentifier: string,
   schemaExp: j.CallExpression | j.MemberExpression | j.Identifier,
   args: j.CallExpression['arguments']
 ) {
+  const extensionObject = args[0];
+  const objectProperties: j.ObjectExpression['properties'] = [];
+
+  // Handle base schema
+  if (schemaExp.type === 'CallExpression' && isInlineObjectCall(schemaExp)) {
+    // Inline the properties from the base schema
+    const baseSchemaProperties = extractObjectProperties(schemaExp);
+    objectProperties.push(...baseSchemaProperties);
+  } else {
+    // Use spread with .entries for schema reference
+    const schemaEntries = j.memberExpression(
+      schemaExp,
+      j.identifier('entries')
+    );
+
+    if (schemaExp.type === 'Identifier') {
+      schemaEntries.comments = [
+        j.block(
+          `@valibot-migrate we can't detect if ${schemaExp.name} has a \`pipe\` operator, if it does you might need to migrate this by hand otherwise it will loose it's pipeline`,
+          true,
+          false
+        ),
+      ];
+    }
+
+    objectProperties.push(j.spreadElement(schemaEntries));
+  }
+
+  // Handle extension object
+  if (extensionObject.type === 'ObjectExpression') {
+    // Inline the extension properties
+    objectProperties.push(...extensionObject.properties);
+  } else if (extensionObject.type !== 'SpreadElement') {
+    // Spread the extension object
+    objectProperties.push(j.spreadElement(extensionObject));
+  } else {
+    objectProperties.push(j.spreadElement(extensionObject.argument));
+  }
+
+  const extendedObject = j.objectExpression(objectProperties);
+
   return j.callExpression(
-    j.memberExpression(j.identifier(valibotIdentifier), j.identifier('extend')),
-    [schemaExp, ...args]
+    j.memberExpression(j.identifier(valibotIdentifier), j.identifier('object')),
+    [extendedObject]
   );
 }
--- codemod/zod-to-valibot/src/transform/schemas-and-links/methods/merge/merge.ts
+++ codemod/zod-to-valibot/src/transform/schemas-and-links/methods/merge/merge.ts
@@ -1,12 +1,97 @@
 import j from 'jscodeshift';
 
+function isInlineObjectCall(exp: j.CallExpression): boolean {
+  return (
+    exp.callee.type === 'MemberExpression' &&
+    exp.callee.property.type === 'Identifier' &&
+    exp.callee.property.name === 'object'
+  );
+}
+
+function extractObjectProperties(
+  exp: j.CallExpression
+): j.ObjectExpression['properties'] {
+  const firstArg = exp.arguments[0];
+  if (firstArg && firstArg.type === 'ObjectExpression') {
+    return firstArg.properties;
+  }
+  return [];
+}
+
 export function transformMerge(
   valibotIdentifier: string,
   schemaExp: j.CallExpression | j.MemberExpression | j.Identifier,
   args: j.CallExpression['arguments']
 ) {
+  const secondSchema = args[0];
+  const objectProperties: j.ObjectExpression['properties'] = [];
+
+  // Handle first schema
+  if (schemaExp.type === 'CallExpression' && isInlineObjectCall(schemaExp)) {
+    // Inline the properties from the first schema
+    const firstSchemaProperties = extractObjectProperties(schemaExp);
+    objectProperties.push(...firstSchemaProperties);
+  } else {
+    // Use spread with .entries for schema reference
+    const firstSchemaEntries = j.memberExpression(
+      schemaExp,
+      j.identifier('entries')
+    );
+
+    if (schemaExp.type === 'Identifier') {
+      firstSchemaEntries.comments = [
+        j.block(
+          `@valibot-migrate we can't detect if ${schemaExp.name} has a \`pipe\` operator, if it does you might need to migrate this by hand otherwise it will loose it's pipeline`,
+          true,
+          false
+        ),
+      ];
+    }
+
+    objectProperties.push(j.spreadElement(firstSchemaEntries));
+  }
+
+  // Handle second schema
+  if (
+    secondSchema.type === 'CallExpression' &&
+    isInlineObjectCall(secondSchema)
+  ) {
+    // Inline the properties from the second schema
+    const secondSchemaProperties = extractObjectProperties(
+      secondSchema as j.CallExpression
+    );
+    objectProperties.push(...secondSchemaProperties);
+    // this can technically never happen cause zod will yell at you if you spread the second element
+  } else if (secondSchema.type !== 'SpreadElement') {
+    // Use spread with .entries for schema reference
+    const secondSchemaEntries = j.memberExpression(
+      secondSchema,
+      j.identifier('entries')
+    );
+
+    if (secondSchema.type === 'Identifier') {
+      secondSchemaEntries.comments = [
+        j.block(
+          `@valibot-migrate we can't detect if ${secondSchema.name} has a \`pipe\` operator, if it does you might need to migrate this by hand otherwise it will loose it's pipeline`,
+          true,
+          false
+        ),
+      ];
+    }
+
+    objectProperties.push(j.spreadElement(secondSchemaEntries));
+  } else {
+    const secondSchemaEntries = j.memberExpression(
+      secondSchema.argument,
+      j.identifier('entries')
+    );
+    objectProperties.push(j.spreadElement(secondSchemaEntries));
+  }
+
+  const mergedObject = j.objectExpression(objectProperties);
+
   return j.callExpression(
-    j.memberExpression(j.identifier(valibotIdentifier), j.identifier('merge')),
-    [schemaExp, ...args]
+    j.memberExpression(j.identifier(valibotIdentifier), j.identifier('object')),
+    [mergedObject]
   );
 }
